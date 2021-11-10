354024b272e2eef7561276ffcd75eb6118917ff0
Natasha Seelam <nseelam1@gmail.com>
2021-11-10 16:12:12 -0500

    fix: makes proper call to the dataloader class

---
+++ test_dataset.py
@@ -0,0 +1,59 @@
+"""
+Unittests to test each dataset example
+
+Update - chemprot + ddi working
+"""
+import os
+import unittest
+from dataloader import BioDataset
+
+# Place your biomedical dataset path here if it doesn't correspond
+dataset_path = "../datasets/"
+
+
+class TestDataset(unittest.TestCase):
+    """Tests ChemProt dataset loaded properly"""
+
+    def test_bc5c5(self):
+        """
+        Test BC5CDR processing
+        """
+        dataset = "bc5cdr"
+        data_root = dataset_path
+
+        dataset = BioDataset(dataset, data_root)
+
+    def test_jnlpba(self):
+        """
+        Test JNLPBA dataset with minimal specifications (dataset + data_root name)
+        """
+        dataset = "jnlpba"
+        data_root = dataset_path
+
+        dataset = BioDataset(dataset, data_root)
+
+    def test_cellfinder(self):
+        """Test CellFinder dataset with only dataset name provided"""
+        dataset = BioDataset("cellfinder")
+
+    def test_linneaus(self):
+        """Test Linnaeus dataset with new data location + all caps"""
+        dataset = "LINNEAUS"
+        data_root = "./mydata"
+        dataset = BioDataset(dataset, data_root)
+
+    def test_ddi(self):
+        """Test DDI downloads + processes properly"""
+        dataset = BioDataset("ddi", dataset_path)
+
+    #def test_drugprot(self):
+    #    """Test DrugProt"""
+    #    dataset = Dataset("drugprot", dataset_path)
+
+    def test_chemprot(self):
+        """Test ChemProt (deprecated; newest version is DrugProt)"""
+        dataset = BioDataset("chemprot", dataset_path)
+
+
+if __name__ == "__main__":
+    unittest.main()
