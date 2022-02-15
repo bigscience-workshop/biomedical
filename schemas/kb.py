ec49bd831883b0087f43eba364f7d2efce035223
Jason Alan Fries <jfries@stanford.edu>
2022-02-14 23:42:50 -0800

    schema changes based on feedback

--- schemas/kb.py
+++ schemas/kb.py
@@ -21,61 +21,61 @@ features = datasets.Features(
     {
         "id": datasets.Value("string"),
         "document_id": datasets.Value("string"),
-        "passages": datasets.Sequence(
+        "passages": [
             {
                 "id": datasets.Value("string"),
                 "type": datasets.Value("string"),
                 "text": datasets.Value("string"),
                 "offsets": datasets.Sequence([datasets.Value("int32")]),
             }
-        ),
-        "entities": datasets.Sequence(
+        ],
+        "entities": [
             {
                 "id": datasets.Value("string"),
                 "offsets": datasets.Sequence([datasets.Value("int32")]),
                 "text": datasets.Sequence([datasets.Value("string")]),
                 "type": datasets.Value("string"),
-                "normalized": datasets.Sequence(
+                "normalized": [
                     {
                         "db_name": datasets.Value("string"),
                         "db_id": datasets.Value("string"),
                     }
-                ),
+                ],
             }
-        ),
-        "events": datasets.Sequence(  # E line in brat
+        ],
+        "events": [
             {
                 "id": datasets.Value("string"),
                 "type": datasets.Value("string"),
                 # refers to the text_bound_annotation of the trigger
                 "trigger": datasets.Value("string"),
-                "arguments": datasets.Sequence(
+                "arguments": [
                     {
                         "role": datasets.Value("string"),
                         "ref_id": datasets.Value("string"),
                     }
-                ),
+                ],
             }
-        ),
-        "coreferences": datasets.Sequence(
+        ],
+        "coreferences": [
             {
                 "id": datasets.Value("string"),
                 "entity_ids": datasets.Sequence(datasets.Value("string")),
             }
-        ),
-        "relations": datasets.Sequence(
+        ],
+        "relations": [
             {
                 "id": datasets.Value("string"),
                 "type": datasets.Value("string"),
                 "arg1_id": datasets.Value("string"),
                 "arg2_id": datasets.Value("string"),
-                "normalized": datasets.Sequence(
+                "normalized": [
                     {
                         "db_name": datasets.Value("string"),
                         "db_id": datasets.Value("string"),
                     }
-                ),
+                ],
             }
-        ),
+        ],
     }
 )
