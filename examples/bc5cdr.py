50246254bf23b92f1ff095043be86f6d76e1efa2
Myungsun Kang <skang@Myungsuns-MacBook-Pro.local>
2022-03-06 08:02:08 -0500

    removing __main__()

--- examples/bc5cdr.py
+++ examples/bc5cdr.py
@@ -321,8 +321,3 @@ class Bc5cdrDataset(datasets.GeneratorBasedBuilder):
                     uid += 1
 
                 yield i, data
-
-if __name__ == "__main__":
-    from datasets import load_dataset
-    dataset = load_dataset(__file__)
-    print(dataset)
