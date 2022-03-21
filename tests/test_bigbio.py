6b9be037edbf5d18aab93b87e207ba278df1f4b9
Leon Weber <leonweber@posteo.de>
2022-03-21 10:12:35 +0100

    Make ref id check work for events

--- tests/test_bigbio.py
+++ tests/test_bigbio.py
@@ -291,10 +291,16 @@ class TestDataLoader(unittest.TestCase):
                 existing_ids.update(self._get_existing_referable_ids(example))
 
                 for ref_id, ref_type in referenced_ids:
-                    if not (ref_id, ref_type) in existing_ids:
-                        logger.warning(
-                            f"Referenced element {(ref_id, ref_type)} could not be found in existing ids {existing_ids}. Please make sure that this is not because of a bug in your data loader."
-                        )
+                    if ref_type == "event":
+                        if not (
+                                (ref_id, "entity") in existing_ids
+                                or (ref_id, "event") in existing_ids
+                        ):
+                            logger.warning(f"Referenced element ({ref_id}, entity/event) could not be found in existing ids {existing_ids}. Please make sure that this is not because of a bug in your data loader.")
+                    else:
+                        if not (ref_id, ref_type) in referenced_ids:
+                            logger.warning(f"Referenced element {(ref_id, ref_type)} could not be found in existing ids {existing_ids}. Please make sure that this is not because of a bug in your data loader.")
+
 
     def test_passages_offsets(self, dataset_bigbio: DatasetDict):
         """
