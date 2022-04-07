ae8ffc20cc9d0a5db7d2fec0c0046b716fdc1ce0
jgolde <jonas.golde@gmail.com>
2022-04-07 14:27:39 +0200

    added bigbio kb schema, ready for review

--- biodatasets/linnaeus/linnaeus.py
+++ biodatasets/linnaeus/linnaeus.py
@@ -14,21 +14,13 @@
 # limitations under the License.
 
 """
-This template serves as a starting point for contributing a dataset to the BigScience Biomedical repo.
+LINNAEUS provides a novel corpus of full-text documents manually annotated for species mentions.
 
-When modifying it for your dataset, look for TODO items that offer specific instructions.
-
-Full documentation on writing dataset loading scripts can be found here:
-https://huggingface.co/docs/datasets/add_dataset.html
-
-To create a dataset loading script you will create a class and implement 3 methods:
-  * `_info`: Establishes the schema for the dataset, and returns a datasets.DatasetInfo object.
-  * `_split_generators`: Downloads and extracts data for each split (e.g. train/val/test) or associate local data with each split.
-  * `_generate_examples`: Creates examples from data on disk that conform to each schema defined in `_info`.
-
-TODO: Before submitting your script, delete this doc string and replace it with a description of your dataset.
-
-[bigbio_schema_name] = (kb, pairs, qa, text, t2t, entailment)
+To understand the true performance of the LINNAEUS system, we generated a gold standard dataset specifically
+annotated to evaluate species name identification software. The reliability of this gold standard is high,
+however some species names are likely to be omitted from this evaluation set, as shown by IAA analysis.
+Performance of species tagging by LINNAEUS on full-text articles is very good, with 94.3% recall and
+97.1% precision on mention level, and 98.1% recall and 90.4% precision on document level.
 """
 
 import os
@@ -65,8 +57,7 @@ _HOMEPAGE = "http://linnaeus.sourceforge.net/"
 _LICENSE = "Creative Commons Attribution 4.0 International (CC BY 4.0)"
 
 _URLS = {
-    "source": "https://sourceforge.net/projects/linnaeus/files/Corpora/manual-corpus-species-1.0.tar.gz/download",
-    "bigbio_text": "https://sourceforge.net/projects/linnaeus/files/Corpora/manual-corpus-species-1.0.tar.gz/download",
+    _DATASETNAME: "https://sourceforge.net/projects/linnaeus/files/Corpora/manual-corpus-species-1.0.tar.gz/download",
 }
 
 _SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION, Tasks.NAMED_ENTITY_DISAMBIGUATION]
@@ -106,8 +97,8 @@ class LinnaeusDataset(datasets.GeneratorBasedBuilder):
         if self.config.schema == "source":
             features = datasets.Features(
                 {
-                    "id": datasets.Value("string"),
                     "document_id": datasets.Value("string"),
+                    "document_type": datasets.Value("string"),
                     "text": datasets.Value("string"),
                     "entities": [
                         {
@@ -138,7 +129,7 @@ class LinnaeusDataset(datasets.GeneratorBasedBuilder):
         )
 
     def _split_generators(self, dl_manager) -> List[datasets.SplitGenerator]:
-        urls = _URLS[self.config.schema]
+        urls = _URLS[_DATASETNAME]
         data_dir = dl_manager.download_and_extract(urls)
 
         return [
@@ -153,23 +144,27 @@ class LinnaeusDataset(datasets.GeneratorBasedBuilder):
     def _generate_examples(self, data_files: Path) -> Tuple[int, Dict]:
         data_path = Path(os.path.join(data_files, "txt"))
         tags_path = Path(os.path.join(data_files, "tags.tsv"))
+        data_files = list(data_path.glob("*txt"))
+        tags = self._load_tags(tags_path)
 
         if self.config.schema == "source":
-            data_files = list(data_path.glob("*txt"))
-            tags = self._load_tags(tags_path)
-            for guid, data_file in enumerate(data_files):
-                document_key = self._extract_document_key(data_file)
-                example = self._create_example(data_file, tags.get(document_key))
+            for data_file in data_files:
+                document_key = data_file.stem
+                if document_key not in tags:
+                    continue
+                example = self._create_source_example(data_file, tags.get(document_key))
                 example["document_id"] = str(document_key)
-                example["id"] = str(guid)
-                breakpoint()
-                yield guid, example
+                yield str(document_key), example
 
-        elif self.config.schema == "bigbio_[bigbio_schema_name]":
-            pass
-            # TODO: yield (key, example) tuples in the bigbio schema
-            #for key, example in thing:
-            #    yield key, example
+        elif self.config.schema == "bigbio_kb":
+            for data_file in data_files:
+                document_key = data_file.stem
+                if document_key not in tags:
+                    continue
+                example = self._create_kb_example(data_file, tags.get(document_key))
+                example["document_id"] = str(document_key)
+                example["id"] = str(document_key)
+                yield str(document_key), example
 
     @staticmethod
     def _load_tags(path: Path) -> Dict:
@@ -191,50 +186,57 @@ class LinnaeusDataset(datasets.GeneratorBasedBuilder):
                     tags[document_id].append(line)
         return tags
 
-    @staticmethod
-    def _extract_document_key(path: Path):
-        return path.stem
-
-    def _create_example(self, txt_file, tags) -> Dict:
+    def _create_source_example(self, txt_file, tags) -> Dict:
         example = {}
         example["entities"] = []
         with open(txt_file, 'r') as file:
             text = file.read()
         example["text"] = text
+        example["document_type"] = "Article"
         for tag_id, tag in enumerate(tags):
-            species_id, start, end, entity_text, normalized_entity_id = tag
+            species_id, start, end, entity_text, _ = tag
+            entity_type, db_name, db_id = species_id.split(":")
             entity = {
-                "id": tag_id,
-                "type": species_id,
-                "text": entity_text,
-                "offsets": [int(start), int(end)],
+                "id": str(tag_id),
+                "type": entity_type,
+                "text": [entity_text],
+                "offsets": [(int(start), int(end))],
                 "normalized": [{
-                    "db_name": self._normalized_entity_lookup(normalized_entity_id),
-                    "db_id": None if normalized_entity_id == "" else normalized_entity_id,
+                    "db_name": db_name,
+                    "db_id": db_id,
                 }]
             }
             example["entities"].append(entity)
         return example
 
-    @staticmethod
-    def _normalized_entity_lookup(normalized_entity_id: str) -> str:
-        normalized_entities = {
-            "0": "misspelling",
-            "1": "OCR error (caused by OCR software when scanning articles into text format)",
-            "2": "incorrect name usage (e.g. using Drosophila when referring to D. melanogaster)",
-            "3": "enumeration (e.g. 'V. vulnificus CMCP6 and YJ016', will result in two mentions)",
-            "4": "modifier (e.g. human in 'human studies', mouse in 'mouse gene', rat in 'rat brain')",
-            "20": "wrong case (e.g. 'drosophila melanogaster')"
-        }
-
-        if normalized_entity_id in normalized_entities:
-            return normalized_entities.get(normalized_entity_id)
-        else:
-            return None
-
-
-
-# This allows you to run your dataloader with `python [dataset_name].py` during development
-## TODO: Remove this before making your PR
-#if __name__ == "__main__":
-#    datasets.load_dataset(__file__)
+    def _create_kb_example(self, txt_file, tags) -> Dict:
+        example = {}
+        with open(txt_file, 'r') as file:
+            text = file.read()
+        # Passages
+        example["passages"] = [{
+            "id": txt_file.stem,
+            "text": [text],
+            "type": "Article",
+            "offsets": [(0, len(text))]
+        }]
+        # Entities
+        example["entities"] = []
+        for tag_id, tag in enumerate(tags):
+            species_id, start, end, entity_text, _ = tag
+            entity_type, db_name, db_id = species_id.split(":")
+            entity = {
+                "id": str(tag_id),
+                "type": entity_type,
+                "text": [entity_text],
+                "offsets": [(int(start), int(end))],
+                "normalized": [{
+                    "db_name": db_name,
+                    "db_id": db_id,
+                }]
+            }
+            example["entities"].append(entity)
+        example["events"] = []
+        example["relations"] = []
+        example["coreferences"] = []
+        return example
