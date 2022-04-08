fa2bba2dcb014a1ae67a06c9ec77e4a3dbc5720c
Fabio Barth <fabio.barth@gmx.de>
2022-04-08 14:12:30 +0200

    add COREF and NER as Task

--- biodatasets/bionlp_st_2011_epi/bionlp_st_2011_epi.py
+++ biodatasets/bionlp_st_2011_epi/bionlp_st_2011_epi.py
@@ -54,7 +54,10 @@ _LICENSE = "DUA"
 _URLs = {"source": "https://github.com/openbiocorpora/bionlp-st-2011-epi/archive/refs/heads/master.zip",
          "bigbio_kb": "https://github.com/openbiocorpora/bionlp-st-2011-epi/archive/refs/heads/master.zip",}
 
-_SUPPORTED_TASKS = [Tasks.EVENT_EXTRACTION,]
+_SUPPORTED_TASKS = [Tasks.EVENT_EXTRACTION,
+                    Tasks.COREFERENCE_RESOLUTION,
+                    Tasks.NAMED_ENTITY_RECOGNITION,
+                    ]
 _SOURCE_VERSION = "1.0.0"
 _BIGBIO_VERSION = "1.0.0"
 
