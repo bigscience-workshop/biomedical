0bf736e997e49d02ce520907e3897d6c5db0713c
John Giorgi <johnmgiorgi@gmail.com>
2022-04-08 10:16:53 -0400

    Add license for PubTator

--- biodatasets/pubtator_central/pubtator_central.py
+++ biodatasets/pubtator_central/pubtator_central.py
@@ -86,7 +86,20 @@ _HOMEPAGE = "https://www.ncbi.nlm.nih.gov/research/pubtator/"
 # Note that this doesn't have to be a common open source license.
 # Some datasets have custom licenses. In this case, simply put the full license terms
 # into `_LICENSE`
-_LICENSE = ""
+_LICENSE = """\
+PUBLIC DOMAIN NOTICE
+National Center for Biotechnology Information
+
+This software/database is a "United States Government Work" under the terms of the United States Copyright Act.
+It was written as part of the authors' official duties as a United States Government employee and thus cannot be
+copyrighted. This software/database is freely available to the public for use. The National Library of Medicine
+and the U.S. Government shave not placed any restriction on its use or reproduction.
+
+Although all reasonable efforts have been taken to ensure the accuracy and reliability of the software and data,
+the NLM and the U.S. Government do not and cannot warrant the performance or results that may be obtained by
+using this software or data. The NLM and the U.S. Government disclaim all warranties, express or implied,
+including warranties of performance, merchantability or fitness for any particular purpose.
+"""
 
 _URLS = {
     "api": "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator",
