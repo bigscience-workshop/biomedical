8f8805ec19abb663e33d82d202af3837bd25935d
jgolde <jonas.golde@gmail.com>
2022-04-09 13:47:52 +0200

    bioscope ready for review

--- biodatasets/bioscope/bioscope.py
+++ biodatasets/bioscope/bioscope.py
@@ -14,24 +14,21 @@
 # limitations under the License.
 
 """
-This template serves as a starting point for contributing a dataset to the BigScience Biomedical repo.
-
-When modifying it for your dataset, look for TODO items that offer specific instructions.
-
-Full documentation on writing dataset loading scripts can be found here:
-https://huggingface.co/docs/datasets/add_dataset.html
-
-To create a dataset loading script you will create a class and implement 3 methods:
-  * `_info`: Establishes the schema for the dataset, and returns a datasets.DatasetInfo object.
-  * `_split_generators`: Downloads and extracts data for each split (e.g. train/val/test) or associate local data with each split.
-  * `_generate_examples`: Creates examples from data on disk that conform to each schema defined in `_info`.
-
-TODO: Before submitting your script, delete this doc string and replace it with a description of your dataset.
-
-[bigbio_schema_name] = (kb, pairs, qa, text, t2t, entailment)
+BioScope
+---
+The corpus consists of three parts, namely medical free texts, biological full papers and biological scientific
+abstracts. The dataset contains annotations at the token level for negative and speculative keywords and at the
+sentence level for their linguistic scope. The annotation process was carried out by two independent linguist
+annotators and a chief linguist – also responsible for setting up the annotation guidelines – who resolved cases
+where the annotators disagreed. The resulting corpus consists of more than 20.000 sentences that were considered
+for annotation and over 10% of them actually contain one (or more) linguistic annotation suggesting negation or
+uncertainty.
 """
 
 import os
+import re
+import xml.etree.ElementTree as ET
+from pathlib import Path
 from typing import List, Tuple, Dict
 
 import datasets
@@ -65,110 +62,111 @@ _HOMEPAGE = "https://rgai.inf.u-szeged.hu/node/105"
 
 _LICENSE = "Creative Commons Attribution 2.0 International (CC BY 2.0)"
 
-# TODO: Add links to the urls needed to download your dataset files.
-#  For local datasets, this variable can be an empty dictionary.
-
-# For publicly available datasets you will most likely end up passing these URLs to dl_manager in _split_generators.
-# In most cases the URLs will be the same for the source and bigbio config.
-# However, if you need to access different files for each config you can have multiple entries in this dict.
-# This can be an arbitrarily nested dict/list of URLs (see below in `_split_generators` method)
 _URLS = {
-    _DATASETNAME: "url or list of urls or ... ",
+    _DATASETNAME: "https://rgai.sed.hu/sites/rgai.sed.hu/files/bioscope.zip",
 }
 
-# TODO: add supported task by dataset. One dataset may support multiple tasks
-_SUPPORTED_TASKS = []  # example: [Tasks.TRANSLATION, Tasks.NAMED_ENTITY_RECOGNITION, Tasks.RELATION_EXTRACTION]
+_SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION]
 
-# TODO: set this to a version that is associated with the dataset. if none exists use "1.0.0"
-#  This version doesn't have to be consistent with semantic versioning. Anything that is
-#  provided by the original dataset as a version goes.
-_SOURCE_VERSION = ""
+_SOURCE_VERSION = "1.0.0"
 
 _BIGBIO_VERSION = "1.0.0"
 
 
-# TODO: Name the dataset class to match the script name using CamelCase instead of snake_case
-#  Append "Dataset" to the class name: BioASQ --> BioasqDataset
-class NewDataset(datasets.GeneratorBasedBuilder):
-    """TODO: Short description of my dataset."""
+class BioscopeDataset(datasets.GeneratorBasedBuilder):
+    """The BioScope corpus consists of medical and biological texts annotated for negation, speculation and their linguistic scope."""
 
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
     BIGBIO_VERSION = datasets.Version(_BIGBIO_VERSION)
 
-    # You will be able to load the "source" or "bigbio" configurations with
-    # ds_source = datasets.load_dataset('my_dataset', name='source')
-    # ds_bigbio = datasets.load_dataset('my_dataset', name='bigbio')
-
-    # For local datasets you can make use of the `data_dir` and `data_files` kwargs
-    # https://huggingface.co/docs/datasets/add_dataset.html#downloading-data-files-and-organizing-splits
-    # ds_source = datasets.load_dataset('my_dataset', name='source', data_dir="/path/to/data/files")
-    # ds_bigbio = datasets.load_dataset('my_dataset', name='bigbio', data_dir="/path/to/data/files")
-
-    # TODO: For each dataset, implement Config for Source and BigBio;
-    #  If dataset contains more than one subset (see examples/bioasq.py) implement for EACH of them.
-    #  Each of them should contain:
-    #   - name: should be unique for each dataset config eg. bioasq10b_(source|bigbio)_[bigbio_schema_name]
-    #   - version: option = (SOURCE_VERSION|BIGBIO_VERSION)
-    #   - description: one line description for the dataset
-    #   - schema: options = (source|bigbio_[bigbio_schema_name])
-    #   - subset_id: subset id is the canonical name for the dataset (eg. bioasq10b)
-    #  where [bigbio_schema_name] = (kb, pairs, qa, text, t2t, entailment)
-
     BUILDER_CONFIGS = [
         BigBioConfig(
-            name="[dataset_name]_source",
+            name="bioscope_source",
             version=SOURCE_VERSION,
-            description="[dataset_name] source schema",
+            description="bioscope source schema",
             schema="source",
-            subset_id="[dataset_name]",
+            subset_id="bioscope",
         ),
         BigBioConfig(
-            name="[dataset_name]_bigbio_[bigbio_schema_name]",
+            name="bioscope_abstracts_source",
+            version=SOURCE_VERSION,
+            description="bioscope source schema",
+            schema="source",
+            subset_id="bioscope_abstracts",
+        ),
+        BigBioConfig(
+            name="bioscope_papers_source",
+            version=SOURCE_VERSION,
+            description="bioscope source schema",
+            schema="source",
+            subset_id="bioscope_papers",
+        ),
+        BigBioConfig(
+            name="bioscope_medical_texts_source",
+            version=SOURCE_VERSION,
+            description="bioscope source schema",
+            schema="source",
+            subset_id="bioscope_medical_texts",
+        ),
+        BigBioConfig(
+            name="bioscope_bigbio_kb",
             version=BIGBIO_VERSION,
-            description="[dataset_name] BigBio schema",
-            schema="bigbio_[bigbio_schema_name]",
-            subset_id="[dataset_name]",
+            description="bioscope BigBio schema",
+            schema="bigbio_kb",
+            subset_id="bioscope",
+        ),
+        BigBioConfig(
+            name="bioscope_abstracts_bigbio_kb",
+            version=BIGBIO_VERSION,
+            description="bioscope BigBio schema",
+            schema="bigbio_kb",
+            subset_id="bioscope_abstracts",
+        ),
+        BigBioConfig(
+            name="bioscope_papers_bigbio_kb",
+            version=BIGBIO_VERSION,
+            description="bioscope BigBio schema",
+            schema="bigbio_kb",
+            subset_id="bioscope_papers",
+        ),
+        BigBioConfig(
+            name="bioscope_medical_texts_bigbio_kb",
+            version=BIGBIO_VERSION,
+            description="bioscope BigBio schema",
+            schema="bigbio_kb",
+            subset_id="bioscope_medical_texts",
         ),
     ]
 
-    DEFAULT_CONFIG_NAME = "[dataset_name]_source"
+    DEFAULT_CONFIG_NAME = "bioscope_source"
 
     def _info(self) -> datasets.DatasetInfo:
 
-        # Create the source schema; this schema will keep all keys/information/labels as close to the original dataset as possible.
-
-        # You can arbitrarily nest lists and dictionaries.
-        # For iterables, use lists over tuples or `datasets.Sequence`
-
         if self.config.schema == "source":
-            # TODO: Create your source schema here
-            raise NotImplementedError()
+            features = datasets.Features(
+               {
+                    "document_id": datasets.Value("string"),
+                    "document_type": datasets.Value("string"),
+                    "text": datasets.Value("string"),
+                    "entities": [
+                        {
+                            "offsets": datasets.Sequence([datasets.Value("int32")]),
+                            "text": datasets.Value("string"),
+                            "type": datasets.Value("string"),
+                            "id": datasets.Value("string"),
+                            "normalized": [
+                                {
+                                    "db_name": datasets.Value("string"),
+                                    "db_id": datasets.Value("string"),
+                                }
+                            ],
+                       }
+                   ],
+                }
+            )
 
-            # EX: Arbitrary NER type dataset
-            # features = datasets.Features(
-            #    {
-            #        "doc_id": datasets.Value("string"),
-            #        "text": datasets.Value("string"),
-            #        "entities": [
-            #            {
-            #                "offsets": [datasets.Value("int64")],
-            #                "text": datasets.Value("string"),
-            #                "type": datasets.Value("string"),
-            #                "entity_id": datasets.Value("string"),
-            #            }
-            #        ],
-            #    }
-            # )
-
-        # Choose the appropriate bigbio schema for your task and copy it here. You can find information on the schemas in the CONTRIBUTING guide.
-
-        # In rare cases you may get a dataset that supports multiple tasks requiring multiple schemas. In that case you can define multiple bigbio configs with a bigbio_[bigbio_schema_name] format.
-
-        # For example bigbio_kb, bigbio_t2t
-        elif self.config.schema == "bigbio_[bigbio_schema_name]":
-            # e.g. features = schemas.kb_features
-            # TODO: Choose your big-bio schema here
-            raise NotImplementedError()
+        elif self.config.schema == "bigbio_kb":
+            features = schemas.kb_features
 
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
@@ -180,84 +178,95 @@ class NewDataset(datasets.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager) -> List[datasets.SplitGenerator]:
         """Returns SplitGenerators."""
-        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
-
-        # If you need to access the "source" or "bigbio" config choice, that will be in self.config.name
-
-        # LOCAL DATASETS: You do not need the dl_manager; you can ignore this argument. Make sure `gen_kwargs` in the return gets passed the right filepath
-
-        # PUBLIC DATASETS: Assign your data-dir based on the dl_manager.
-
-        # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLs; many examples use the download_and_extract method; see the DownloadManager docs here: https://huggingface.co/docs/datasets/package_reference/builder_classes.html#datasets.DownloadManager
-
-        # dl_manager can accept any type of nested list/dict and will give back the same structure with the url replaced with the path to local files.
-
-        # TODO: KEEP if your dataset is PUBLIC; remove if not
         urls = _URLS[_DATASETNAME]
         data_dir = dl_manager.download_and_extract(urls)
-
-        # TODO: KEEP if your dataset is LOCAL; remove if NOT
-        if self.config.data_dir is None:
-            raise ValueError("This is a local dataset. Please pass the data_dir kwarg to load_dataset.")
-        else:
-            data_dir = self.config.data_dir
-
-        # Not all datasets have predefined canonical train/val/test splits.
-        # If your dataset has no predefined splits, use datasets.Split.TRAIN for all of the data.
 
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
-                # Whatever you put in gen_kwargs will be passed to _generate_examples
                 gen_kwargs={
-                    "filepath": os.path.join(data_dir, "train.jsonl"),
-                    "split": "train",
+                    "data_files": data_dir,
                 },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.TEST,
-                gen_kwargs={
-                    "filepath": os.path.join(data_dir, "test.jsonl"),
-                    "split": "test",
-                },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.VALIDATION,
-                gen_kwargs={
-                    "filepath": os.path.join(data_dir, "dev.jsonl"),
-                    "split": "dev",
-                },
-            ),
+            )
         ]
 
-    # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
-
-    # TODO: change the args of this function to match the keys in `gen_kwargs`. You may add any necessary kwargs.
-
-    def _generate_examples(self, filepath, split: str) -> Tuple[int, Dict]:
+    def _generate_examples(self, data_files: Path) -> Tuple[int, Dict]:
         """Yields examples as (key, example) tuples."""
-        # TODO: This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
-
-        # The `key` is for legacy reasons (tfds) and is not important in itself, but must be unique for each example.
-
-        # NOTE: For local datasets you will have access to self.config.data_dir and self.config.data_files
-
+        sentences = self._load_sentences(data_files)
         if self.config.schema == "source":
-            # TODO: yield (key, example) tuples in the original dataset schema
-            for key, example in thing:
-                yield key, example
+            for guid, sentence_tuple in enumerate(sentences):
+                document_type, sentence = sentence_tuple
+                example = self._create_example(sentence_tuple)
+                example["document_type"] = f"{document_type}_{sentence.attrib['id']}"
+                example["text"] = "".join(sentence_tuple[1].itertext())
+                yield guid, example
 
-        elif self.config.schema == "bigbio_[bigbio_schema_name]":
-            # TODO: yield (key, example) tuples in the bigbio schema
-            for key, example in thing:
-                yield key, example
+        elif self.config.schema == "bigbio_kb":
+            for guid, sentence_tuple in enumerate(sentences):
+                document_type, sentence = sentence_tuple
+                example = self._create_example(sentence_tuple)
+                example["id"] = guid
+                example["passages"] = [{
+                    "id": sentence.attrib['id'],
+                    "type": document_type,
+                    "text": ["".join(sentence.itertext())],
+                    "offsets": [(0, len("".join(sentence.itertext())))]
+                }]
+                example["events"] = []
+                example["coreferences"] = []
+                example["relations"] = []
+                yield guid, example
 
+    def _load_sentences(self, data_files: Path) -> List:
+        """
+        Returns a list of tuples (Document type, iterator from dataset)
+        """
+        if self.config.subset_id.__contains__("abstracts"):
+            sentences = self._concat_iterators(("Abstract", ET.parse(os.path.join(data_files, "abstracts.xml")).getroot().iter("sentence")))
+        elif self.config.subset_id.__contains__("papers"):
+            sentences = self._concat_iterators(("Paper", ET.parse(os.path.join(data_files, "full_papers.xml")).getroot().iter("sentence")))
+        elif self.config.subset_id.__contains__("medical_texts"):
+            sentences = self._concat_iterators(("Medical text", ET.parse(os.path.join(data_files, "clinical_merger/clinical_records_anon.xml")).getroot().iter("sentence")))
+        else:
+            abstracts = ET.parse(os.path.join(data_files, "abstracts.xml")).getroot().iter("sentence")
+            papers = ET.parse(os.path.join(data_files, "full_papers.xml")).getroot().iter("sentence")
+            medical_texts = ET.parse(os.path.join(data_files, "clinical_merger/clinical_records_anon.xml")).getroot().iter(
+                "sentence")
+            sentences = self._concat_iterators(("Abstract", abstracts), ("Paper", papers), ("Medical text", medical_texts))
+        return sentences
 
-# This template is based on the following template from the datasets package:
-# https://github.com/huggingface/datasets/blob/master/templates/new_dataset_script.py
+    @staticmethod
+    def _concat_iterators(*iterator_tuple):
+        for document_type, iterator in iterator_tuple:
+            for element in iterator:
+                yield document_type, element
 
+    def _create_example(self, sentence_tuple):
+        document_type, sentence = sentence_tuple
+        document_type_prefix = document_type[0]
 
-# This allows you to run your dataloader with `python [dataset_name].py` during development
-# TODO: Remove this before making your PR
-if __name__ == "__main__":
-    datasets.load_dataset(__file__)
+        example = {}
+        example["document_id"] = f"{document_type_prefix}_{sentence.attrib['id']}"
+        example["entities"] = self._extract_entities(sentence, document_type_prefix)
+        return example
+
+    def _extract_entities(self, sentence, document_type_prefix):
+        text = "".join(sentence.itertext())
+        entities = []
+        xcopes = dict([(xcope.attrib["id"], xcope) for xcope in sentence.iter("xcope")])
+        cues = dict([(cue.attrib["ref"], cue) for cue in sentence.iter("cue")])
+        for idx, xcope in xcopes.items():
+            # X2.140.2 has no annotation in raw data
+            if cues.get(idx) == None:
+                continue
+            entities.append({
+                "id": f"{document_type_prefix}_{idx}",
+                "type": cues.get(idx).attrib["type"],
+                "text": ["".join(xcope.itertext())],
+                "offsets": self._extract_offsets(text=text, entity_text="".join(xcope.itertext())),
+                "normalized": []
+            })
+        return entities
+
+    def _extract_offsets(self, text, entity_text):
+        return [(text.find(entity_text), text.find(entity_text) + len(entity_text))]
