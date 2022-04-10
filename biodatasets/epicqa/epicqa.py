0e15e01f35e3393a7d71ac8a0500a0a0be50570f
Labrak Yanis <yanis.labrak@alumni.univ-avignon.fr>
2022-04-11 00:51:46 +0200

    Update loading script

--- biodatasets/epicqa/epicqa.py
+++ biodatasets/epicqa/epicqa.py
@@ -32,6 +32,26 @@ Answering (EPIC-QA) track challenges teams to develop systems
 capable of automatically answering ad-hoc questions about the
 disease COVID-19, its causal virus SARS-CoV-2, related corona
 viruses, and the recommended response to the pandemic.
+The 2020 EPIC-QA track involves two tasks:
+
+Task A
+    Expert QA: In Task A, teams are provided with a set of
+    questions asked by experts and are asked to provide a
+    ranked list of expert-level answers to each question.
+    In Task A, answers should provide information that is
+    useful to researchers, scientists, or clinicians.
+
+Task B
+    Consumer QA: In Task B, teams are provided with a set
+    of questions asked by consumers and are asked to provide
+    a ranked list of consumer-friendly answers to each question.
+    In Task B, answers should be understandable by the general
+    public.
+
+While each task will have its own set of questions, many of the
+questions will overlap. This is by design, so that the collection
+can be used to explore whether the same approaches or systems can
+account for different types of users.
 """
 
 _DATASETNAME = "EpicQA"
@@ -43,18 +63,18 @@ _LICENSE = "DUA-NC"
 _BASE = "https://bionlp.nlm.nih.gov/epic_qa/data/epic_qa_"
 
 _URLs = {
-    "epicqa_research_articles_source": _BASE + "cord_2020-10-22-split-corrected.tar.gz",
-    "epicqa_research_articles_bigbio_qa": _BASE + "cord_2020-10-22-split-corrected.tar.gz",
+    "epicqa_research_source": _BASE + "cord_2020-10-22-split-corrected.tar.gz",
+    "epicqa_research_bigbio_qa": _BASE + "cord_2020-10-22-split-corrected.tar.gz",
     
-    "epicqa_consumer_articles_source": _BASE + "consumer_2020-11-02.tar.gz",
-    "epicqa_consumer_articles_bigbio_qa": _BASE + "consumer_2020-11-02.tar.gz",
+    "epicqa_consumer_source": _BASE + "consumer_2020-11-02.tar.gz",
+    "epicqa_consumer_bigbio_qa": _BASE + "consumer_2020-11-02.tar.gz",
 }
 
 _DIRS = {
-    "research_articles" : [
+    "research" : [
         "2020-10-22-new/",
     ],
-    "consumer_articles" : [
+    "consumer" : [
         "epic_qa_consumer_2020_11-02/ask_science-2020-10-29/",
         "epic_qa_consumer_2020_11-02/ccns-trec/",
         "epic_qa_consumer_2020_11-02/chqa-2020-10-09/",
@@ -68,49 +88,49 @@ _BIGBIO_VERSION = "1.0.0"
 class EpicQaDataset(datasets.GeneratorBasedBuilder):
     """EpicQA"""
 
-    DEFAULT_CONFIG_NAME = "source"
+    DEFAULT_CONFIG_NAME = "epicqa_research_source"
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
     BIGBIO_VERSION = datasets.Version(_BIGBIO_VERSION)
 
     BUILDER_CONFIGS = [
 
         BigBioConfig(
-            name="epicqa_research_articles_source",
+            name="epicqa_research_source",
             version=SOURCE_VERSION,
             description="EpicQA source schema for the Research Articles subset",
             schema="source",
-            subset_id="epicqa_research_articles",
+            subset_id="epicqa_research",
         ),
 
         BigBioConfig(
-            name="epicqa_consumer_articles_source",
+            name="epicqa_consumer_source",
             version=SOURCE_VERSION,
             description="EpicQA source schema for the Consumer Articles subset",
             schema="source",
-            subset_id="epicqa_consumer_articles",
+            subset_id="epicqa_consumer",
         ),
 
         BigBioConfig(
-            name="epicqa_research_articles_bigbio_qa",
+            name="epicqa_research_bigbio_qa",
             version=BIGBIO_VERSION,
             description="EpicQA BigBio schema for the Research Articles subset",
             schema="bigbio_qa",
-            subset_id="epicqa_research_articles",
+            subset_id="epicqa_research",
         ),
 
         BigBioConfig(
-            name="epicqa_consumer_articles_bigbio_qa",
+            name="epicqa_consumer_bigbio_qa",
             version=BIGBIO_VERSION,
             description="EpicQA BigBio schema for the Consumer Articles subset",
             schema="bigbio_qa",
-            subset_id="epicqa_consumer_articles",
+            subset_id="epicqa_consumer",
         ),
 
     ]
 
     def _info(self):
 
-        if self.config.name == "epicqa_consumer_articles_source":
+        if self.config.name == "epicqa_consumer_source":
 
             features = datasets.Features(
                 {
@@ -136,7 +156,7 @@ class EpicQaDataset(datasets.GeneratorBasedBuilder):
                 }
             )
         
-        elif self.config.name == "epicqa_research_articles_source":
+        elif self.config.name == "epicqa_research_source":
 
             features = datasets.Features(
                 {
@@ -197,7 +217,12 @@ class EpicQaDataset(datasets.GeneratorBasedBuilder):
     def _generate_examples(self, filepath, split):
         """Yields examples as (key, example) tuples."""
 
-        subset_name = "research_articles" if "research_articles" in self.config.name else "consumer_articles"
+        if self.config.subset_id.endswith("research"):
+            subset_name = "research"
+        elif self.config.subset_id.endswith("consumer"):
+            subset_name = "consumer"
+        else:
+            raise ValueError(f"subset_id {self.config.subset_id} does not end with 'research' or 'consumer'")
 
         uid = 0
 
@@ -221,7 +246,7 @@ class EpicQaDataset(datasets.GeneratorBasedBuilder):
                     doc = json.load(file)
 
                     # Generate source schema for the consumers articles
-                    if self.config.name == "epicqa_consumer_articles_source":
+                    if self.config.name == "epicqa_consumer_source":
 
                         yield uid, {
                             "document_id": doc["document_id"],
@@ -247,8 +272,10 @@ class EpicQaDataset(datasets.GeneratorBasedBuilder):
                             ],
                         }
                     
+                        uid += 1
+
                     # Generate source schema for the research articles
-                    elif self.config.name == "epicqa_research_articles_source":
+                    elif self.config.name == "epicqa_research_source":
 
                         yield uid, {
                             "document_id": doc["document_id"],
@@ -276,20 +303,34 @@ class EpicQaDataset(datasets.GeneratorBasedBuilder):
                             ],
                         }
 
+                        uid += 1
+
                     # Generate the BigBio schema
                     else:
 
-                        doc["type"] = "list"
+                        contexts = []
 
-                        yield uid, {
-                            "id": doc["document_id"],
-                            "document_id": doc["document_id"],
-                            "question_id": doc["document_id"],
-                            "question": doc["metadata"]["title"],
-                            "type": doc["type"],
-                            "context": doc["metadata"]["title"],
-                            "answer": [c["text"] for c in doc["contexts"]],
-                            "choices": [],
-                        }
+                        # For each contexts
+                        for c in doc["contexts"]:
 
-                    uid += 1
+                            spans = []
+
+                            # For each span                            
+                            for s in c["sentences"]:
+
+                                spans.append(
+                                    c["text"][int(s["start"]):int(s["end"])]
+                                )
+
+                            yield uid, {
+                                "id": c["context_id"],
+                                "document_id": doc["document_id"],
+                                "question_id": c["context_id"],
+                                "question": doc["metadata"]["title"],
+                                "type": "list",
+                                "context": c["text"],
+                                "answer": spans,
+                                "choices": [],
+                            }
+
+                            uid += 1
