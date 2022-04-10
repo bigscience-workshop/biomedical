77e25d71cd32674f8f936bbab9d924e965eaf6d8
Gabriel Altay <gabriel.altay@gmail.com>
2022-04-10 16:50:56 -0400

    some constants to represent missing and null values

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,11 @@
 from enum import Enum
 
 
+class BigBioValues(Enum):
+    MISSING = "<BB-MISSING-VALUE>"  # to represent data that is not present in a dataset
+    NULL = "<BB-NULL-VALUE">        # to represent data that is present in a dataset but has null meaning
+
+
 class Tasks(Enum):
     NAMED_ENTITY_RECOGNITION = "NER"
     NAMED_ENTITY_DISAMBIGUATION = "NED"
