7b31b47f21bcba98dadacc6658a515f8c2a8fd10
John Giorgi <johnmgiorgi@gmail.com>
2022-04-12 19:17:48 -0400

    Fix typo in license

--- biodatasets/pubtator_central/pubtator_central.py
+++ biodatasets/pubtator_central/pubtator_central.py
@@ -87,7 +87,7 @@ National Center for Biotechnology Information
 This software/database is a "United States Government Work" under the terms of the United States Copyright Act.
 It was written as part of the authors' official duties as a United States Government employee and thus cannot be
 copyrighted. This software/database is freely available to the public for use. The National Library of Medicine
-and the U.S. Government shave not placed any restriction on its use or reproduction.
+and the U.S. Government have not placed any restriction on its use or reproduction.
 
 Although all reasonable efforts have been taken to ensure the accuracy and reliability of the software and data,
 the NLM and the U.S. Government do not and cannot warrant the performance or results that may be obtained by
