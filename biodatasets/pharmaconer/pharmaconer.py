14c90a27d44690e876b266beaf146cc85947e9c8
Marc Pàmies <mpamies247@gmail.com>
2022-04-12 15:26:31 +0200

    update pharmaconer.py to include subtrack 2

--- biodatasets/pharmaconer/pharmaconer.py
+++ biodatasets/pharmaconer/pharmaconer.py
@@ -23,11 +23,10 @@ shared task on detecting drug and chemical entities in Spanish medical documents
 """
 
 import os
-from pathlib import Path
-from typing import Dict, List, Tuple
-
 import datasets
-
+import pandas as pd
+from pathlib import Path
+from typing import List, Tuple, Dict
 from utils import parsing, schemas
 from utils.configs import BigBioConfig
 from utils.constants import Tasks
@@ -54,23 +53,44 @@ _CITATION = """\
 
 _DATASETNAME = "pharmaconer"
 
-_DESCRIPTION = """\
+_GENERAL_DESCRIPTION = """\
 PharmaCoNER: Pharmacological Substances, Compounds and Proteins Named Entity Recognition track
 
 This dataset is designed for the PharmaCoNER task, sponsored by Plan de Impulso de las Tecnologías del Lenguaje.
 
-It is a manually classified collection of clinical case studies derived from the
-Spanish Clinical Case Corpus (SPACCC), an open access electronic library that gathers
-Spanish medical publications from SciELO (Scientific Electronic Library Online).
+It is a manually classified collection of clinical case studies derived from the Spanish Clinical \
+Case Corpus (SPACCC), an open access electronic library that gathers Spanish medical publications \
+from SciELO (Scientific Electronic Library Online).
 
-The annotation of the entire set of entity mentions was carried out by medicinal chemistry experts
+The annotation of the entire set of entity mentions was carried out by medicinal chemistry experts \
 and it includes the following 4 entity types: NORMALIZABLES, NO_NORMALIZABLES, PROTEINAS and UNCLEAR.
 
-The PharmaCoNER corpus contains a total of 396,988 words and 1,000 clinical cases that
-have been randomly sampled into 3 subsets: train (500), validation (250) and test (250).
+The PharmaCoNER corpus contains a total of 396,988 words and 1,000 clinical cases that have been \
+randomly sampled into 3 subsets. The training set contains 500 clinical cases, while the development \
+and test sets contain 250 clinical cases each.
 
 For further information, please visit https://temu.bsc.es/pharmaconer/ or send an email to encargo-pln-life@bsc.es
 """
+
+_DESCRIPTION_SUBTRACK_1 = """\
+\n\nSUBTRACK 1: NER offset and entity type classification\n
+The first subtrack consists in the classical entity-based or instanced-based evaluation that requires \
+that system outputs match exactly the beginning and end locations of each entity tag, as well as match \
+the entity annotation type of the gold standard annotations.
+"""
+
+_DESCRIPTION_SUBTRACK_2 = """\
+\n\nSUBTRACK 2: CONCEPT INDEXING\n
+In the second subtask, a list of unique SNOMED concept identifiers have to be generated for each document. \
+The predictions are compared to the manually annotated concept ids corresponding to chemical compounds and \
+pharmacological substances.
+"""
+
+_DESCRIPTIONS = {
+    "subtrack_1": _GENERAL_DESCRIPTION + _DESCRIPTION_SUBTRACK_1,
+    "subtrack_2": _GENERAL_DESCRIPTION + _DESCRIPTION_SUBTRACK_2,
+    "full_task":  _GENERAL_DESCRIPTION + _DESCRIPTION_SUBTRACK_1 + _DESCRIPTION_SUBTRACK_2,
+}
 
 _HOMEPAGE = "https://temu.bsc.es/pharmaconer/index.php/datasets/"
 
@@ -80,12 +100,11 @@ _URLS = {
     "pharmaconer": "https://zenodo.org/record/4270158/files/pharmaconer.zip?download=1",
 }
 
-_SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION]
+_SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION, Tasks.TEXT_CLASSIFICATION]
 
 _SOURCE_VERSION = "1.1.0"
 
 _BIGBIO_VERSION = "1.0.0"
-
 
 class PharmaconerDataset(datasets.GeneratorBasedBuilder):
     """Manually annotated collection of clinical case studies from Spanish medical publications."""
@@ -95,24 +114,36 @@ class PharmaconerDataset(datasets.GeneratorBasedBuilder):
 
     BUILDER_CONFIGS = [
         BigBioConfig(
-            name="pharmaconer_source",
-            version=SOURCE_VERSION,
-            description="PharmaCoNER source schema",
-            schema="source",
-            subset_id="pharmaconer",
+                name="pharmaconer_source",
+                version=SOURCE_VERSION,
+                description="PharmaCoNER source schema",
+                schema="source",
+                subset_id="full_task",
         ),
         BigBioConfig(
             name="pharmaconer_bigbio_kb",
             version=BIGBIO_VERSION,
             description="PharmaCoNER BigBio schema",
             schema="bigbio_kb",
-            subset_id="pharmaconer",
+            subset_id="subtrack_1",
+        ),
+        BigBioConfig(
+            name="pharmaconer_bigbio_text",
+            version=BIGBIO_VERSION,
+            description="PharmaCoNER BigBio schema",
+            schema="bigbio_text",
+            subset_id="subtrack_2",
         ),
     ]
 
     DEFAULT_CONFIG_NAME = "pharmaconer_source"
 
-    _ENTITY_TYPES = {"NORMALIZABLES", "PROTEINAS", "NO_NORMALIZABLES", "UNCLEAR"}
+    _ENTITY_TYPES = {
+        'NORMALIZABLES',
+        'PROTEINAS',
+        'NO_NORMALIZABLES',
+        'UNCLEAR'
+    }
 
     def _info(self) -> datasets.DatasetInfo:
 
@@ -122,6 +153,7 @@ class PharmaconerDataset(datasets.GeneratorBasedBuilder):
                     "id": datasets.Value("string"),
                     "document_id": datasets.Value("string"),
                     "text": datasets.Value("string"),
+                    "labels": [datasets.Value("string")], # subtrack 2 codes
                     "text_bound_annotations": [  # T line in brat
                         {
                             "offsets": datasets.Sequence([datasets.Value("int32")]),
@@ -178,7 +210,9 @@ class PharmaconerDataset(datasets.GeneratorBasedBuilder):
                             "ref_id": datasets.Value("string"),
                             "resource_name": datasets.Value("string"),
                             "cuid": datasets.Value("string"),
-                            "text": datasets.Value("string"),
+                            "text": datasets.Value(
+                                "string"
+                            ),
                         }
                     ],
                 },
@@ -187,8 +221,11 @@ class PharmaconerDataset(datasets.GeneratorBasedBuilder):
         elif self.config.schema == "bigbio_kb":
             features = schemas.kb_features
 
+        elif self.config.schema == "bigbio_text":
+            features = schemas.text_features
+
         return datasets.DatasetInfo(
-            description=_DESCRIPTION,
+            description=_DESCRIPTIONS[self.config.subset_id],
             features=features,
             homepage=_HOMEPAGE,
             license=_LICENSE,
@@ -209,45 +246,76 @@ class PharmaconerDataset(datasets.GeneratorBasedBuilder):
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
                 gen_kwargs={
-                    "filepath": Path(os.path.join(data_dir, "pharmaconer/train-set_1.1/train/subtrack1")),
+                    "filepaths": [Path(os.path.join(data_dir, "pharmaconer/train-set_1.1/train/subtrack1")),
+                                 Path(os.path.join(data_dir, "pharmaconer/train-set_1.1/train/subtrack2"))],
                     "split": "train",
                 },
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.TEST,
                 gen_kwargs={
-                    "filepath": Path(os.path.join(data_dir, "pharmaconer/test-set_1.1/test/subtrack1")),
+                    "filepaths": [Path(os.path.join(data_dir, "pharmaconer/test-set_1.1/test/subtrack1")),
+                                 Path(os.path.join(data_dir, "pharmaconer/test-set_1.1/test/subtrack2"))],
                     "split": "test",
                 },
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.VALIDATION,
                 gen_kwargs={
-                    "filepath": Path(os.path.join(data_dir, "pharmaconer/dev-set_1.1/dev/subtrack1")),
+                    "filepaths": [Path(os.path.join(data_dir, "pharmaconer/dev-set_1.1/dev/subtrack1")),
+                                 Path(os.path.join(data_dir, "pharmaconer/dev-set_1.1/dev/subtrack2"))],
                     "split": "dev",
                 },
             ),
         ]
 
-    def _generate_examples(self, filepath, split: str) -> Tuple[int, Dict]:
+    def _generate_examples(self, filepaths, split: str) -> Tuple[int, Dict]:
         """
         This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
         Method parameters are unpacked from `gen_kwargs` as given in `_split_generators`.
         """
 
-        txt_files = list(filepath.glob("*txt"))
+        txt_files = list(filepaths[0].glob("*txt"))
+        tsv_files = list(filepaths[1].glob("*tsv"))
 
         if self.config.schema == "source":
-            for guid, txt_file in enumerate(txt_files):
+            for guid, (txt_file, tsv_file) in enumerate(zip(txt_files, tsv_files)):
                 example = parsing.parse_brat_file(txt_file)
+                try:
+                    subtrack2_df = pd.read_csv(tsv_file, sep="\t", header=None)
+                    subtrack2_df[1] = subtrack2_df[1].apply(str)
+                    codes_set = set(subtrack2_df[1].unique().flatten())
+                    codes_set.discard("<null>")
+                    example['labels'] = list(codes_set)
+                except Exception: # subtrack 2 has no codes for this document
+                    example['labels'] = []               
                 example["id"] = str(guid)
                 yield guid, example
+
         elif self.config.schema == "bigbio_kb":
-            for guid, txt_file in enumerate(txt_files):
-                example = parsing.brat_parse_to_bigbio_kb(
-                    parsing.parse_brat_file(txt_file), entity_types=self._ENTITY_TYPES
-                )
+            for guid, (txt_file, tsv_file) in enumerate(zip(txt_files, tsv_files)):
+                example = parsing.brat_parse_to_bigbio_kb(parsing.parse_brat_file(txt_file), entity_types=self._ENTITY_TYPES)
                 example["id"] = str(guid)
                 yield guid, example
+
+        elif self.config.schema == "bigbio_text":
+            for guid, (txt_file, tsv_file) in enumerate(zip(txt_files, tsv_files)):
+                brat = parsing.brat_parse_to_bigbio_kb(parsing.parse_brat_file(txt_file), entity_types=self._ENTITY_TYPES)
+                try:
+                    subtrack2_df = pd.read_csv(tsv_file, sep="\t", header=None)
+                    subtrack2_df[1] = subtrack2_df[1].apply(str)
+                    codes_set = set(subtrack2_df[1].unique().flatten())
+                    codes_set.discard("<null>")
+                    labels = list(codes_set)
+                except Exception: # subtrack 2 has no codes for this document
+                    labels = []
+                example = {
+                    "id": str(guid),
+                    "document_id": brat["document_id"],
+                    "text": brat["passages"][0]["text"][0],
+                    "labels": labels,
+                }
+                yield guid, example
+
         else:
             raise ValueError(f"Invalid config: {self.config.name}")
