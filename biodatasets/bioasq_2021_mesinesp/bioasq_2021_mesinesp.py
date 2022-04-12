11e523866b13cc27f739fe3ddb2e7d6ea933f028
Nicholas Broad <nicholas@nmbroad.com>
2022-04-12 01:27:00 +0000

    change subset_id
    fix formatting

--- biodatasets/bioasq_2021_mesinesp/bioasq_2021_mesinesp.py
+++ biodatasets/bioasq_2021_mesinesp/bioasq_2021_mesinesp.py
@@ -14,32 +14,58 @@
 # limitations under the License.
 
 """
-The main aim of MESINESP2 is to promote the development of practically relevant semantic indexing tools for biomedical content in non-English language. We have generated a manually annotated corpus, where domain experts have labeled a set of scientific literature, clinical trials, and patent abstracts. All the documents were labeled with DeCS descriptors, which is a structured controlled vocabulary created by BIREME to index scientific publications on BvSalud, the largest database of scientific documents in Spanish, which hosts records from the databases LILACS, MEDLINE, IBECS, among others. 
+The main aim of MESINESP2 is to promote the development of practically relevant
+semantic indexing tools for biomedical content in non-English language.
+We have generated a manually annotated corpus, where domain experts have
+labeled a set of scientific literature, clinical trials, and patent abstracts.
+All the documents were labeled with DeCS descriptors, which is a structured controlled
+vocabulary created by BIREME to index scientific publications on BvSalud, the largest
+database of scientific documents in Spanish, which hosts records from the databases
+LILACS, MEDLINE, IBECS, among others.
 
-MESINESP track at BioASQ9 explores the efficiency of systems for assigning DeCS to different types of biomedical documents. To that purpose, we have divided the task into three subtracks depending on the document type. Then, for each one we generated an annotated corpus which was provided to participating teams:
+MESINESP track at BioASQ9 explores the efficiency of systems for assigning
+DeCS to different types of biomedical documents. To that purpose, we have
+divided the task into three subtracks depending on the document type. Then, for
+each one we generated an annotated corpus which was provided to participating teams:
 
-[Subtrack 1 corpus] MESINESP-L – Scientific Literature: It contains all Spanish records from LILACS and IBECS databases at the Virtual Health Library (VHL) with non-empty abstract written in Spanish.
-[Subtrack 2 corpus] MESINESP-T- Clinical Trials contains records from Registro Español de Estudios Clínicos (REEC). REEC doesn't provide documents with the structure title/abstract needed in BioASQ, for that reason we have built artificial abstracts based on the content available in the data crawled using the REEC API. 
-[Subtrack 3 corpus] MESINESP-P – Patents: This corpus includes patents in Spanish extracted from Google Patents which have the IPC code “A61P” and “A61K31”.
-In addition, we also provide a set of complementary data such as: the DeCS terminology file, a silver standard with the participants' predictions to the task background set and the entities of medications, diseases, symptoms and medical procedures extracted from the BSC NERs documents.
+[Subtrack 1 corpus] MESINESP-L – Scientific Literature: It contains all
+Spanish records from LILACS and IBECS databases at the Virtual Health Library
+(VHL) with non-empty abstract written in Spanish.
+[Subtrack 2 corpus] MESINESP-T- Clinical Trials contains records from Registro
+Español de Estudios Clínicos (REEC). REEC doesn't provide documents with the
+structure title/abstract needed in BioASQ, for that reason we have built
+artificial abstracts based on the content available in the data crawled using the REEC API.
+[Subtrack 3 corpus] MESINESP-P – Patents: This corpus includes patents in
+Spanish extracted from Google Patents which have the IPC code “A61P” and “A61K31”.
+In addition, we also provide a set of complementary data such as: the DeCS
+terminology file, a silver standard with the participants' predictions to the task
+background set and the entities of medications, diseases, symptoms and medical
+procedures extracted from the BSC NERs documents.
 """
 
-import os
 import json
-from typing import List, Tuple, Dict
+import os
+from typing import Dict, List, Tuple
 
 import datasets
+
 from utils import schemas
 from utils.configs import BigBioConfig
 from utils.constants import Tasks
 
 _CITATION = """\
 @conference {396,
-	title = {Overview of BioASQ 2021-MESINESP track. Evaluation of advance hierarchical classification techniques for scientific literature, patents and clinical trials.},
-	booktitle = {Proceedings of the 9th BioASQ Workshop A challenge on large-scale biomedical semantic indexing and question answering},
-	year = {2021},
-	url = {http://ceur-ws.org/Vol-2936/paper-11.pdf},
-	author = {Gasco, Luis and Nentidis, Anastasios and Krithara, Anastasia and Estrada-Zavala, Darryl and Toshiyuki Murasaki, Renato and Primo-Pe{\~n}a, Elena and Bojo-Canales, Cristina and Paliouras, Georgios and Krallinger, Martin}
+    title = {Overview of BioASQ 2021-MESINESP track. Evaluation of
+    advance hierarchical classification techniques for scientific
+    literature, patents and clinical trials.},
+    booktitle = {Proceedings of the 9th BioASQ Workshop
+    A challenge on large-scale biomedical semantic indexing
+    and question answering},
+    year = {2021},
+    url = {http://ceur-ws.org/Vol-2936/paper-11.pdf},
+    author = {Gasco, Luis and Nentidis, Anastasios and Krithara, Anastasia
+     and Estrada-Zavala, Darryl and Toshiyuki Murasaki, Renato and Primo-Pe{\~n}a,
+     Elena and Bojo-Canales, Cristina and Paliouras, Georgios and Krallinger, Martin}
 }
 
 """
@@ -47,15 +73,34 @@ _CITATION = """\
 _DATASETNAME = "bioasq_2021_mesinesp"
 
 
-_DESCRIPTION = """\
-The main aim of MESINESP2 is to promote the development of practically relevant semantic indexing tools for biomedical content in non-English language. We have generated a manually annotated corpus, where domain experts have labeled a set of scientific literature, clinical trials, and patent abstracts. All the documents were labeled with DeCS descriptors, which is a structured controlled vocabulary created by BIREME to index scientific publications on BvSalud, the largest database of scientific documents in Spanish, which hosts records from the databases LILACS, MEDLINE, IBECS, among others. 
+_DESCRIPTION = """
+The main aim of MESINESP2 is to promote the development of practically relevant
+semantic indexing tools for biomedical content in non-English language.
+We have generated a manually annotated corpus, where domain experts have
+labeled a set of scientific literature, clinical trials, and patent abstracts.
+All the documents were labeled with DeCS descriptors, which is a structured controlled
+vocabulary created by BIREME to index scientific publications on BvSalud, the largest
+database of scientific documents in Spanish, which hosts records from the databases
+LILACS, MEDLINE, IBECS, among others.
 
-MESINESP track at BioASQ9 explores the efficiency of systems for assigning DeCS to different types of biomedical documents. To that purpose, we have divided the task into three subtracks depending on the document type. Then, for each one we generated an annotated corpus which was provided to participating teams:
+MESINESP track at BioASQ9 explores the efficiency of systems for assigning
+DeCS to different types of biomedical documents. To that purpose, we have
+divided the task into three subtracks depending on the document type. Then, for
+each one we generated an annotated corpus which was provided to participating teams:
 
-[Subtrack 1 corpus] MESINESP-L – Scientific Literature: It contains all Spanish records from LILACS and IBECS databases at the Virtual Health Library (VHL) with non-empty abstract written in Spanish.
-[Subtrack 2 corpus] MESINESP-T- Clinical Trials contains records from Registro Español de Estudios Clínicos (REEC). REEC doesn't provide documents with the structure title/abstract needed in BioASQ, for that reason we have built artificial abstracts based on the content available in the data crawled using the REEC API. 
-[Subtrack 3 corpus] MESINESP-P – Patents: This corpus includes patents in Spanish extracted from Google Patents which have the IPC code “A61P” and “A61K31”.
-In addition, we also provide a set of complementary data such as: the DeCS terminology file, a silver standard with the participants' predictions to the task background set and the entities of medications, diseases, symptoms and medical procedures extracted from the BSC NERs documents.
+[Subtrack 1 corpus] MESINESP-L – Scientific Literature: It contains all
+Spanish records from LILACS and IBECS databases at the Virtual Health Library
+(VHL) with non-empty abstract written in Spanish.
+[Subtrack 2 corpus] MESINESP-T- Clinical Trials contains records from Registro
+Español de Estudios Clínicos (REEC). REEC doesn't provide documents with the
+structure title/abstract needed in BioASQ, for that reason we have built
+artificial abstracts based on the content available in the data crawled using the REEC API.
+[Subtrack 3 corpus] MESINESP-P – Patents: This corpus includes patents in
+Spanish extracted from Google Patents which have the IPC code “A61P” and “A61K31”.
+In addition, we also provide a set of complementary data such as: the DeCS
+terminology file, a silver standard with the participants' predictions to the task
+background set and the entities of medications, diseases, symptoms and medical
+procedures extracted from the BSC NERs documents.
 """
 
 _HOMEPAGE = "https://zenodo.org/record/5602914#.YhSXJ5PMKWt"
@@ -72,7 +117,7 @@ _URLS = {
 }
 
 
-_SUPPORTED_TASKS = [Tasks.TEXT_CLASSIFICATION] 
+_SUPPORTED_TASKS = [Tasks.TEXT_CLASSIFICATION]
 
 _SOURCE_VERSION = "1.0.6"
 
@@ -80,7 +125,8 @@ _BIGBIO_VERSION = "1.0.0"
 
 
 class Bioasq2021MesinespDataset(datasets.GeneratorBasedBuilder):
-    """A dataset to promote the development of practically relevant semantic indexing tools for biomedical content in non-English language."""
+    """A dataset to promote the development of practically relevant
+    semantic indexing tools for biomedical content in non-English language."""
 
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
     BIGBIO_VERSION = datasets.Version(_BIGBIO_VERSION)
@@ -91,49 +137,49 @@ class Bioasq2021MesinespDataset(datasets.GeneratorBasedBuilder):
             version=SOURCE_VERSION,
             description="bioasq_2021_mesinesp source schema subtrack1",
             schema="source",
-            subset_id="bioasq_2021_mesinesp",
+            subset_id="bioasq_2021_mesinesp_subtrack1_all",
         ),
         BigBioConfig(
             name="bioasq_2021_mesinesp_subtrack1_only_articles_source",
             version=SOURCE_VERSION,
             description="bioasq_2021_mesinesp source schema subtrack1",
             schema="source",
-            subset_id="bioasq_2021_mesinesp",
+            subset_id="bioasq_2021_mesinesp_subtrack1_only_articles",
         ),
         BigBioConfig(
             name="bioasq_2021_mesinesp_subtrack2_source",
             version=SOURCE_VERSION,
             description="bioasq_2021_mesinesp source schema subtrack2",
             schema="source",
-            subset_id="bioasq_2021_mesinesp",
+            subset_id="bioasq_2021_mesinesp_subtrack2",
         ),
         BigBioConfig(
             name="bioasq_2021_mesinesp_subtrack3_source",
             version=SOURCE_VERSION,
             description="bioasq_2021_mesinesp source schema subtrack3",
             schema="source",
-            subset_id="bioasq_2021_mesinesp",
+            subset_id="bioasq_2021_mesinesp_subtrack3",
         ),
         BigBioConfig(
             name="bioasq_2021_mesinesp_subtrack1_bigbio_text",
             version=BIGBIO_VERSION,
             description="bioasq_2021_mesinesp BigBio schema subtrack1",
             schema="bigbio_text",
-            subset_id="bioasq_2021_mesinesp",
+            subset_id="bioasq_2021_mesinesp_subtrack1",
         ),
         BigBioConfig(
             name="bioasq_2021_mesinesp_subtrack2_bigbio_text",
             version=BIGBIO_VERSION,
             description="bioasq_2021_mesinesp BigBio schema subtrack2",
             schema="bigbio_text",
-            subset_id="bioasq_2021_mesinesp",
+            subset_id="bioasq_2021_mesinesp_subtrack2",
         ),
         BigBioConfig(
             name="bioasq_2021_mesinesp_subtrack3_bigbio_text",
             version=BIGBIO_VERSION,
             description="bioasq_2021_mesinesp BigBio schema subtrack3",
             schema="bigbio_text",
-            subset_id="bioasq_2021_mesinesp",
+            subset_id="bioasq_2021_mesinesp_subtrack3",
         ),
     ]
 
@@ -141,29 +187,18 @@ class Bioasq2021MesinespDataset(datasets.GeneratorBasedBuilder):
 
     def _info(self) -> datasets.DatasetInfo:
 
-        # Create the source schema; this schema will keep all keys/information/labels as close to the original dataset as possible.
-
-        # You can arbitrarily nest lists and dictionaries.
-        # For iterables, use lists over tuples or `datasets.Sequence`
-
         if self.config.schema == "source":
-                features = datasets.Features(
-                    {
-                            "abstractText": datasets.Value("string"),
-                            "db":datasets.Value("string"),
-                            "decsCodes": datasets.Sequence(datasets.Value("string")),
-                            "id":datasets.Value("string"),
-                            "journal":datasets.Value("string"),
-                            "title":datasets.Value("string"),
-                            "year": datasets.Value("string"),
-                    }
-                )
-
-        # Choose the appropriate bigbio schema for your task and copy it here. You can find information on the schemas in the CONTRIBUTING guide.
-
-        # In rare cases you may get a dataset that supports multiple tasks requiring multiple schemas. In that case you can define multiple bigbio configs with a bigbio_[bigbio_schema_name] format.
-
-        # For example bigbio_kb, bigbio_t2t
+            features = datasets.Features(
+                {
+                    "abstractText": datasets.Value("string"),
+                    "db": datasets.Value("string"),
+                    "decsCodes": datasets.Sequence(datasets.Value("string")),
+                    "id": datasets.Value("string"),
+                    "journal": datasets.Value("string"),
+                    "title": datasets.Value("string"),
+                    "year": datasets.Value("string"),
+                }
+            )
         elif self.config.schema == "bigbio_text":
             features = schemas.text.features
 
@@ -189,8 +224,10 @@ class Bioasq2021MesinespDataset(datasets.GeneratorBasedBuilder):
         if self.config.data_dir is None:
             try:
                 data_dir = dl_manager.download_and_extract(urls)
-            except:
-                raise ConnectionError("Could not download. Save locally and use `data_dir` kwarg")
+            except ConnectionError:
+                raise ConnectionError(
+                    "Could not download. Save locally and use `data_dir` kwarg"
+                )
         else:
             data_dir = self.config.data_dir
 
@@ -215,21 +252,26 @@ class Bioasq2021MesinespDataset(datasets.GeneratorBasedBuilder):
         split_gens = [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
-                # Whatever you put in gen_kwargs will be passed to _generate_examples
                 gen_kwargs={
-                    "filepath": os.path.join(data_dir, top_folder, "Train", train_filepath),
+                    "filepath": os.path.join(
+                        data_dir, top_folder, "Train", train_filepath
+                    ),
                 },
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.VALIDATION,
                 gen_kwargs={
-                    "filepath": os.path.join(data_dir, top_folder, "Development", dev_filepath),
+                    "filepath": os.path.join(
+                        data_dir, top_folder, "Development", dev_filepath
+                    ),
                 },
             ),
             datasets.SplitGenerator(
                 name=datasets.Split.TEST,
                 gen_kwargs={
-                    "filepath": os.path.join(data_dir, top_folder, "Test", test_filepath),
+                    "filepath": os.path.join(
+                        data_dir, top_folder, "Test", test_filepath
+                    ),
                 },
             ),
         ]
@@ -240,12 +282,11 @@ class Bioasq2021MesinespDataset(datasets.GeneratorBasedBuilder):
 
         return split_gens
 
-
     def _generate_examples(self, filepath) -> Tuple[int, Dict]:
         """Yields examples as (key, example) tuples."""
 
         if self.config.schema == "source":
-            
+
             with open(filepath) as fp:
                 data = json.load(fp)
 
@@ -258,11 +299,8 @@ class Bioasq2021MesinespDataset(datasets.GeneratorBasedBuilder):
 
             for key, example in enumerate(data["articles"]):
                 yield key, {
-                        "id": example["id"],
-                        "document_id": "NULL",
-                        "text": example["abstractText"],
-                        "labels": example["decsCodes"],
+                    "id": example["id"],
+                    "document_id": "NULL",
+                    "text": example["abstractText"],
+                    "labels": example["decsCodes"],
                 }
-
-if __name__ == "__main__":
-    datasets.load_dataset(__file__)
