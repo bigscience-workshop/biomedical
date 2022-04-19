3f25116ee08591bbbdfbc775a2ffae46216d52e0
Patrick Haller <patrickhaller40@googlemail.com>
2022-04-19 13:35:34 +0200

    Finished data loader for kb

--- biodatasets/nagel/nagel.py
+++ biodatasets/nagel/nagel.py
@@ -15,15 +15,17 @@
 
 
 import os
+from pathlib import Path
 from typing import List
 
+
 import datasets
+import pandas as pd
 
 from utils import schemas
 from utils.configs import BigBioConfig
 from utils.constants import Tasks
 
-# TODO: Add BibTeX citation
 _CITATION = """\
 @article{,
   author    = {Kevin Nagel},
@@ -40,16 +42,12 @@ _CITATION = """\
 
 _DATASETNAME = "nagel"
 
-# You can copy an official description
 _DESCRIPTION = """\
-This dataset is designed for XXX NLP task.
+A set of 100 abstracts annotated by Kevin Nagel with protein, residue, organism triples.
 """
 
 _HOMEPAGE = "https://sourceforge.net/projects/bionlp-corpora/files/ProteinResidue/"
 
-# Note that this doesn't have to be a common open source license.
-# Some datasets have custom licenses. In this case, simply put the full license terms
-# into `_LICENSE`
 _LICENSE = """
 Copyright (c) 2011 Kevin Nagel
 Permission is hereby granted, free of charge, to any person obtaining a copy of this
@@ -73,12 +71,9 @@ _URLS = {
     _DATASETNAME: "https://sourceforge.net/projects/bionlp-corpora/files/ProteinResidue/NagelCorpus.tar.gz/download",
 }
 
-_SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION, Tasks.RELATION_EXTRACTION]
+_SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION]  # , Tasks.RELATION_EXTRACTION]
 
-# this version doesn't have to be consistent with semantic versioning. Anything that is
-# provided by the original dataset as a version goes.
 _SOURCE_VERSION = "1.0.0"
-
 _BIGBIO_VERSION = "1.0.0"
 
 
@@ -101,40 +96,40 @@ class NagelDataset(datasets.GeneratorBasedBuilder):
             description="Nagel BigBio schema",
             schema="bigbio_kb",
             subset_id="nagel",
-        )
+        ),
     ]
 
     DEFAULT_CONFIG_NAME = "nagel_source"
 
     def _info(self) -> datasets.DatasetInfo:
 
-        # Create the source schema; this schema will keep all keys/information/labels as close to the original dataset as possible.
-
-        # You can arbitrarily nest lists and dictionaries.
-        # For iterables, use lists over tuples or `datasets.Sequence`
-
         if self.config.schema == "source":
             features = datasets.Features(
-               {
-                   "id": datasets.Value("string"),
-                   "document_id": datasets.Value("string"),
-                   "text": datasets.Value("string"),
-                   "xml_annotated_text": datasets.Value("string"),
-                   "entities": [
-                       {
-                           "offsets": [datasets.Value("int64")],
-                           "type": datasets.Value("string"),
-                           "AminoAcid_WildType_3_Letter_Abbrev": datasets.Value("string"),
-                           "Residue_Position": datasets.Value("string"),
-                           "AminoAcid_MutatedType_3_Letter_Abbrev": datasets.Value("string"),
-                           "Residue_mention_in_original_text": datasets.Value("string"),
-                       }
-                   ],
-               }
+                {
+                    "id": datasets.Value("string"),
+                    "document_id": datasets.Value("string"),
+                    "text": datasets.Value("string"),
+                    "xml_annotated_text": datasets.Value("string"),
+                    "entities": [
+                        {
+                            "offsets": [datasets.Value("int64")],
+                            "type": datasets.Value("string"),
+                            "AminoAcid_WildType_3_Letter_Abbrev": datasets.Value(
+                                "string"
+                            ),
+                            "Residue_Position": datasets.Value("string"),
+                            "AminoAcid_MutatedType_3_Letter_Abbrev": datasets.Value(
+                                "string"
+                            ),
+                            "Residue_mention_in_original_text": datasets.Value(
+                                "string"
+                            ),
+                        }
+                    ],
+                }
             )
 
-        # For example bigbio_kb, bigbio_t2t
-        elif self.config.schema =="bigbio_kb":
+        elif self.config.schema == "bigbio_kb":
             features = schemas.kb_features
 
         return datasets.DatasetInfo(
@@ -150,17 +145,18 @@ class NagelDataset(datasets.GeneratorBasedBuilder):
         urls = _URLS[_DATASETNAME]
         data_dir = dl_manager.download_and_extract(urls)
 
-        # Not all datasets have predefined canonical train/val/test splits. If your dataset does not have any splits, you can omit any missing splits.
-        # If your dataset has no predefined splits, use datasets.Split.TRAIN for all of the data.
-        
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
                 # Whatever you put in gen_kwargs will be passed to _generate_examples
                 gen_kwargs={
                     "filepath": os.path.join(data_dir, "NagelCorpus", "Nagel_GC.xml"),
-                    "filepath_standoff": os.path.join(data_dir, "NagelCorpus", "Nagel_GC.standoff.txt"),
-                    "folder_path": os.path.join(data_dir, "NagelCorpus", "NagelCorpusText"),
+                    "filepath_standoff": os.path.join(
+                        data_dir, "NagelCorpus", "Nagel_GC.standoff.txt"
+                    ),
+                    "folder_path": os.path.join(
+                        data_dir, "NagelCorpus", "NagelCorpusText"
+                    ),
                     "split": "train",
                 },
             ),
@@ -168,37 +164,31 @@ class NagelDataset(datasets.GeneratorBasedBuilder):
 
     # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
 
-    def _generate_examples(self, filepath, filepath_standoff, folder_path, split) -> (int, dict):
-       
-        from pprint import pprint
-        from pathlib import Path
+    def _generate_examples(
+        self, filepath, filepath_standoff, folder_path, split
+    ) -> (int, dict):
 
-        from utils.parsing import parse_brat_file
-
-        import pandas as pd
-        
         so_annot = pd.read_csv(
-                filepath_standoff,
-                delimiter="\t",
-                names=[
-                    "PMID",
-                    "AnnotationType",
-                    "Span_start",
-                    "Span_End",
-                    "AminoAcid_WildType_3_Letter_Abbrev",
-                    "ResiduePosition",
-                    "AminoAcid_MutatedType_3_Letter_Abbrev",
-                    "Residue_mention_in_original_text"
-                ]
+            filepath_standoff,
+            delimiter="\t",
+            names=[
+                "PMID",
+                "AnnotationType",
+                "Span_start",
+                "Span_End",
+                "AminoAcid_WildType_3_Letter_Abbrev",
+                "ResiduePosition",
+                "AminoAcid_MutatedType_3_Letter_Abbrev",
+                "Residue_mention_in_original_text",
+            ],
         )
-        
+
         import xml.etree.ElementTree as ET
 
-            
         # XML annotated abstract, but not a read correct xml tree
         # Abstract are divided by a extra newline
         with open(filepath) as f:
-        
+
             # xml_abstracts = f.read().split("\n\n")
             xml_abstracts = "<root>" + f.read() + "</root>"
 
@@ -210,11 +200,17 @@ class NagelDataset(datasets.GeneratorBasedBuilder):
             for child in root:
 
                 if child.tag == "rel":
-                    current_abstract.append(str(ET.tostring(child, encoding="utf-8", method="xml")))
+                    current_abstract.append(
+                        str(ET.tostring(child, encoding="utf-8", method="xml"))
+                    )
 
                 if child.tag == "abs":
-                    current_abstract.append(str(ET.tostring(child, encoding="utf-8", method="xml")))
-                    xml_annotations[int(child.attrib["id"])] = "\n".join(current_abstract)
+                    current_abstract.append(
+                        str(ET.tostring(child, encoding="utf-8", method="xml"))
+                    )
+                    xml_annotations[int(child.attrib["id"])] = "\n".join(
+                        current_abstract
+                    )
                     current_abstract = []
 
         if self.config.schema == "source":
@@ -227,67 +223,90 @@ class NagelDataset(datasets.GeneratorBasedBuilder):
                     text = f.read()
 
                 pmid = int(file.name.removesuffix(".txt"))
-                    
+
                 # Can PMID corresponding annotations
                 doc_annotations = so_annot[so_annot.PMID == pmid]
 
                 entities = []
                 for annot in doc_annotations.to_dict("records"):
 
-                    entities.append({
-                        "offsets": [annot["Span_start"], annot["Span_End"]],
-                        "type": annot["AnnotationType"],
-                        "AminoAcid_WildType_3_Letter_Abbrev": annot["AminoAcid_WildType_3_Letter_Abbrev"],
-                        "Residue_Position": annot["ResiduePosition"],
-                        "AminoAcid_MutatedType_3_Letter_Abbrev": annot["AminoAcid_MutatedType_3_Letter_Abbrev"],
-                        "Residue_mention_in_original_text": annot["Residue_mention_in_original_text"]
-
-                    })
+                    entities.append(
+                        {
+                            "offsets": [annot["Span_start"], annot["Span_End"]],
+                            "type": annot["AnnotationType"],
+                            "AminoAcid_WildType_3_Letter_Abbrev": annot[
+                                "AminoAcid_WildType_3_Letter_Abbrev"
+                            ],
+                            "Residue_Position": annot["ResiduePosition"],
+                            "AminoAcid_MutatedType_3_Letter_Abbrev": annot[
+                                "AminoAcid_MutatedType_3_Letter_Abbrev"
+                            ],
+                            "Residue_mention_in_original_text": annot[
+                                "Residue_mention_in_original_text"
+                            ],
+                        }
+                    )
                 yield pmid, {
                     "id": pmid,
                     "document_id": pmid,
                     "text": text,
                     "entities": entities,
-                    "xml_annotated_text": xml_annotations[pmid]
+                    "xml_annotated_text": xml_annotations[pmid],
                 }
 
-
         elif self.config.schema == "bigbio_kb":
-            pass
 
-            # for file in Path(folder_path).iterdir():
-            #
-            #     if file.suffix != ".txt":
-            #         continue
-            #
-            #     with open(file) as f:
-            #         text = f.read()
-            #
-            #     pmid = int(file.name.removesuffix(".txt"))
-            #         
-            #     # Can PMID corresponding annotations
-            #     doc_annotations = so_annot[so_annot.PMID == pmid]
-            #
-            #     entities = []
-            #     for annot in doc_annotations.to_dict("records"):
-            #
-            #         entities.append({
-            #             "id": str(pmid),
-            #             "offsets": [annot["Span_start"], annot["Span_End"]],
-            #             "type": annot["AnnotationType"],
-            #             "text": [text]
-            #             # "normalized": []
-            #
-            #         })
-            #
-            #
-            #     yield pmid, {
-            #         "id": str(pmid),
-            #         "document_id": str(pmid),
-            #         "entities": entities,
-            #         "passages": [],
-            #         # "events": [],
-            #         # "coreferences": [],
-            #         # "relations": []
-            #     }
+            entity_id = 0
+            passage_id = 0
 
+            for file in Path(folder_path).iterdir():
+
+                if file.suffix != ".txt":
+                    continue
+
+                with open(file) as f:
+                    text = f.read()
+
+                pmid = int(file.name.removesuffix(".txt"))
+
+                # Can PMID corresponding annotations
+                doc_annotations = so_annot[so_annot.PMID == pmid]
+
+                entities = []
+
+                for annot in doc_annotations.to_dict("records"):
+
+                    # Convert word span to char span
+                    span_start = len(" ".join(text.split(" ")[: annot["Span_start"]]))
+                    span_end = len(" ".join(text.split(" ")[: annot["Span_End"]]))
+
+                    entities.append(
+                        {
+                            "id": entity_id,
+                            "offsets": [(span_start, span_end)],
+                            "type": annot["AnnotationType"],
+                            "text": [text[span_start:span_end]],
+                            "normalized": [],
+                        }
+                    )
+
+                    entity_id += 1
+
+                passage_id += 1
+
+                yield str(pmid), {
+                    "id": str(pmid),
+                    "document_id": str(pmid),
+                    "entities": entities,
+                    "passages": [
+                        {
+                            "id": str(passage_id) + "_text",
+                            "offsets": [[0, len(text)]],
+                            "type": "abstract",
+                            "text": [text],
+                        }
+                    ],
+                    "events": [],
+                    "coreferences": [],
+                    "relations": [],
+                }
