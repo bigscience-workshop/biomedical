5e3cd39cec1ef7177f01306fd4448583e436f594
Natasha Seelam <nseelam1@gmail.com>
2022-04-28 09:45:40 -0400

    ft: adds generic fxn that inherits BB config defaults

--- bigbio/utils/configs.py
+++ bigbio/utils/configs.py
@@ -1,5 +1,6 @@
-from dataclasses import dataclass
-
+from dataclasses import dataclass, asdict
+from functools import partial
+from typing import List, Callable
 import datasets
 
 
@@ -12,3 +13,21 @@ class BigBioConfig(datasets.BuilderConfig):
     description: str = None
     schema: str = None
     subset_id: str = None
+
+
+def default_BigBioConfig(
+    DEFAULT_CONFIG_NAME: str,
+    BUILDER_CONFIGS: List[BigBioConfig],
+    ) -> Callable:
+    """
+    If config_args specified (i.e. local datasets), create a partial class for `BUILDER_CONFIG_CLASS` that will inherit the default view.
+    
+    Args:
+        DEFAULT_CONFIG_NAME: Name of the default config
+        BUILDER_CONFIGS: Default configs available in the dataclass
+    """  # noqa
+    for config in BUILDER_CONFIGS:
+        if config.name == DEFAULT_CONFIG_NAME:
+            config_args = asdict(config)
+
+    return partial(BigBioConfig, **config_args)
