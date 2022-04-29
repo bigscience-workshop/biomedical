c6e9933ea8e7140fd52912a34eeb8a7484e27a24
Gabriel Altay <gabriel.altay@gmail.com>
2022-04-29 10:43:22 -0400

    Minimal make package (not from fork) (#520)
    * make a pip installable package biomed_datasets
    
    * rm src directory idea
    
    * drop n2c2_2014, was there for PR review
    
    * remove requirements.txt
    
    * added basic setup.py and deploy requirements in setup.cfg
    
    * change package name to bigbio

--- biodatasets/bionlp_st_2011_epi/bionlp_st_2011_epi.py
+++ biodatasets/bionlp_st_2011_epi/bionlp_st_2011_epi.py
@@ -19,9 +19,9 @@ from typing import List
 
 import datasets
 
-from utils import parsing, schemas
-from utils.configs import BigBioConfig
-from utils.constants import Tasks
+from bigbio.utils import parsing, schemas
+from bigbio.utils.configs import BigBioConfig
+from bigbio.utils.constants import Tasks
 
 _DATASETNAME = "bionlp_st_2011_epi"
 _SOURCE_VIEW_NAME = "source"
