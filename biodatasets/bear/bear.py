bf2f5ae9a000039f17859754a909480c5ea73f7e
Karthik Rangasai <karthikrangasai@gmail.com>
2022-04-29 20:52:05 +0530

    Change imports to use new bigbio module.

--- biodatasets/bear/bear.py
+++ biodatasets/bear/bear.py
@@ -19,9 +19,9 @@ from typing import Dict, List, Tuple, Union
 import datasets
 import jsonlines
 
-from utils import schemas
-from utils.configs import BigBioConfig
-from utils.constants import Tasks
+from bigbio.utils import schemas
+from bigbio.utils.configs import BigBioConfig
+from bigbio.utils.constants import Tasks
 
 _CITATION = """\
 @InProceedings{wuehrl_klinger_2022,
