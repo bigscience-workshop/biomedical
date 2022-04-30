99b73351eb95e5256f8a1365de5c41f18f880db7
simon <simonott@gmx.at>
2022-04-30 10:38:39 +0200

    Fixed imports of 10 datasets

--- biodatasets/scicite/scicite.py
+++ biodatasets/scicite/scicite.py
@@ -35,9 +35,9 @@ from typing import Dict, List, Tuple
 import datasets
 import numpy as np
 
-from utils import schemas
-from utils.configs import BigBioConfig
-from utils.constants import Tasks
+from bigbio.utils import schemas
+from bigbio.utils.configs import BigBioConfig
+from bigbio.utils.constants import Tasks
 
 _CITATION = """\
 @inproceedings{cohan:naacl19,
