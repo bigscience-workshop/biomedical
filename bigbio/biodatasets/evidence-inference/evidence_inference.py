7005bec0158a0cfc38194d28d10c351a2f694dfd
Gabriel Altay <gabriel.altay@gmail.com>
2022-05-22 19:18:20 -0400

    add _LOCAL=False to evidence inference
    add _LOCAL=False to evidence inference

--- bigbio/biodatasets/evidence-inference/evidence_inference.py
+++ bigbio/biodatasets/evidence-inference/evidence_inference.py
@@ -34,6 +34,7 @@ from bigbio.utils import schemas
 from bigbio.utils.configs import BigBioConfig
 from bigbio.utils.constants import Tasks
 
+_LOCAL = False
 _CITATION = """\
 @inproceedings{deyoung-etal-2020-evidence,
     title = "Evidence Inference 2.0: More Data, Better Models",
