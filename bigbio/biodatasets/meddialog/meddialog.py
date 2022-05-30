22fb18eb45d25bd01f8dfc2a4231c503fefc1fc6
Gabriel Altay <gabriel.altay@gmail.com>
2022-05-30 14:31:54 -0400

    fixing typo in top comment encoding line

--- bigbio/biodatasets/meddialog/meddialog.py
+++ bigbio/biodatasets/meddialog/meddialog.py
@@ -1,4 +1,4 @@
-# coding=utf-8medd
+# coding=utf-8
 # Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
