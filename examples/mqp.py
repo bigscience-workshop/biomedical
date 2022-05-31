a0a266a05d6f9c94159b8d5ed9a8ead3ef3a76dd
sg-wbi <87170658+sg-wbi@users.noreply.github.com>
2022-05-31 10:09:44 +0200

    Fix _PUBMED in examples (#666)
    * mlee: standardize roles
    
    * Revert "mlee: standardize roles"
    
    This reverts commit cd27b130e39ecc4d720c59d1a7b411bbdc2633d2.
    
    * fix _PUBMED value

--- examples/mqp.py
+++ examples/mqp.py
@@ -29,7 +29,7 @@ from bigbio.utils.configs import BigBioConfig
 from bigbio.utils.constants import Lang, Tasks
 
 _LANGUAGES = [Lang.EN]
-_PUBMED = True
+_PUBMED = False
 _LOCAL = False
 _CITATION = """\
 @article{DBLP:journals/biodb/LiSJSWLDMWL16,
