c2267bf26cade20739217aebe371c8037b2f0926
Gabriel Altay <gabriel.altay@gmail.com>
2022-06-08 23:30:20 -0400

    add pubmed bool to several datasets (#705)

--- bigbio/biodatasets/genia_ptm_event_corpus/genia_ptm_event_corpus.py
+++ bigbio/biodatasets/genia_ptm_event_corpus/genia_ptm_event_corpus.py
@@ -33,6 +33,7 @@ from bigbio.utils.constants import Lang, Tasks
 from bigbio.utils.license import Licenses
 
 _LANGUAGES = [Lang.EN]
+_PUBMED = True
 _LOCAL = False
 _CITATION = """\
 @inproceedings{ohta-etal-2010-event,
