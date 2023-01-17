f0c1eb98e3ee91ba84988477af9d67e43c0e82c6
Ignacio Llorca <llorcarodriguez@uni-potsdam.de>
2023-01-17 20:00:30 +0100

    Adapt GGPONC2 loader to new contributing guidelines

--- bigbio/biodatasets/ggponc2/ggponc2.py
+++ bigbio/biodatasets/ggponc2/ggponc2.py
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 import json
-import os
-from typing import Dict, List, Tuple
 from pathlib import Path
+from typing import Dict, List, Tuple
+
 import datasets
 import pandas as pd
 from bigbio.utils import schemas
@@ -69,9 +69,10 @@ _PUBMED = False
 _SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION]
 _SOURCE_VERSION = "2.0.0"
 _BIGBIO_VERSION = "1.0.0"
-_LICENSE = Licenses.DUA
-_DISPLAYNAME = 'GGPONC 2.0'
+_DISPLAYNAME = "GGPONC 2.0"
 _DATASETNAME = "ggponc2"
+_LICENSE = Licenses.DUA
+
 
 class GgponcDataset(datasets.GeneratorBasedBuilder):
 
@@ -118,6 +119,7 @@ class GgponcDataset(datasets.GeneratorBasedBuilder):
             description=_DESCRIPTION,
             features=features,
             homepage=_HOMEPAGE,
+            license=_LICENSE,
             citation=_CITATION,
         )
 
@@ -134,10 +136,14 @@ class GgponcDataset(datasets.GeneratorBasedBuilder):
         split_dir = data_dir / "annotations/splits.csv"
 
         dir_lookup = {
-            "ggponc2_fine_long_bigbio_kb": data_dir / "annotations/json/fine/long/all.json",
-            "ggponc2_fine_short_bigbio_kb": data_dir / "annotations/json/fine/short/all.json",
-            "ggponc2_coarse_long_bigbio_kb": data_dir / "annotations/json/coarse/long/all.json",
-            "ggponc2_coarse_short_bigbio_kb": data_dir / "annotations/json/coarse/short/all.json",
+            "ggponc2_fine_long_bigbio_kb": data_dir
+            / "annotations/json/fine/long/all.json",
+            "ggponc2_fine_short_bigbio_kb": data_dir
+            / "annotations/json/fine/short/all.json",
+            "ggponc2_coarse_long_bigbio_kb": data_dir
+            / "annotations/json/coarse/long/all.json",
+            "ggponc2_coarse_short_bigbio_kb": data_dir
+            / "annotations/json/coarse/short/all.json",
         }
 
         data_dir = dir_lookup[self.config.name]
@@ -174,7 +180,6 @@ class GgponcDataset(datasets.GeneratorBasedBuilder):
     ) -> Tuple[int, Dict]:
         """Yields examples as (key, example) tuples."""
         splits = pd.read_csv(split_dir)
-        counter = 1
 
         with open(filepath, encoding="utf8") as file:
             data = json.load(file)
@@ -192,12 +197,14 @@ class GgponcDataset(datasets.GeneratorBasedBuilder):
 
                     for j, passage in enumerate(row["passages"]):
                         passage_id = passage["id"]
-                        out["passages"].append({
-                            "id": f"{uid}-{passage_id}-{j}",
-                            "type": passage["type"],
-                            "text": [passage["text"]],
-                            "offsets": passage["offsets"],
-                        })
+                        out["passages"].append(
+                            {
+                                "id": f"{uid}-{passage_id}-{j}",
+                                "type": passage["type"],
+                                "text": [passage["text"]],
+                                "offsets": passage["offsets"],
+                            }
+                        )
 
                     for i, _ in enumerate(out["entities"]):
                         out["entities"][i]["id"] = f"{uid}-{i}"
@@ -208,4 +215,3 @@ class GgponcDataset(datasets.GeneratorBasedBuilder):
                     out["relations"] = []
 
                     yield uid, out
-
