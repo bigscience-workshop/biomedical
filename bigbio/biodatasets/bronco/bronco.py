037d5cf0fbf63b960efd48e22ea352296a9c6ad3
Ignacio Llorca <llorcarodriguez@uni-potsdam.de>
2023-01-22 21:22:50 +0100

    BRONCO loader

--- bigbio/biodatasets/bronco/bronco.py
+++ bigbio/biodatasets/bronco/bronco.py
@@ -18,8 +18,7 @@ from typing import List, Tuple, Dict
 
 import datasets
 from .bigbiohub import BigBioConfig
-from bigbio.utils import schemas
-from .bigbiohub import Tasks
+from bigbio.utils.constants import Tasks, Lang
 from bigbio.utils.license import Licenses
 
 import xml.etree.ElementTree as ET
@@ -49,7 +48,7 @@ _DESCRIPTION = """\
 BRONCO150 is a corpus containing selected sentences of 150 German discharge summaries of cancer patients (hepatocelluar carcinoma or melanoma) treated at Charite Universitaetsmedizin Berlin or Universitaetsklinikum Tuebingen. All discharge summaries were manually anonymized. The original documents were scrambled at the sentence level to make reconstruction of individual reports impossible.
 """
 _HOMEPAGE = "https://www2.informatik.hu-berlin.de/~leser/bronco/index.html"
-# _LICENSE = Licenses.DUA
+_LICENSE = Licenses.DUA
 _URLS = {}
 _PUBMED = False
 _SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION, Tasks.NAMED_ENTITY_DISAMBIGUATION]
@@ -57,6 +56,7 @@ _SOURCE_VERSION = "1.0.0"
 _BIGBIO_VERSION = "1.0.0"
 _DATASETNAME = "bronco"
 _DISPLAYNAME = "BRONCO"
+_LANGUAGES = [Lang.DE]
 
 
 class Bronco(datasets.GeneratorBasedBuilder):
@@ -137,13 +137,12 @@ class Bronco(datasets.GeneratorBasedBuilder):
             )
 
         elif self.config.schema == "bigbio_kb":
-            features = schemas.kb_features
+            features = kb_features
 
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
             features=features,
             homepage=_HOMEPAGE,
-            # license=_LICENSE,
             citation=_CITATION,
         )
 
@@ -166,114 +165,133 @@ class Bronco(datasets.GeneratorBasedBuilder):
     def _generate_examples(self, filepath, split: str) -> Tuple[int, Dict]:
         """Yields examples as (key, example) tuples."""
 
-        tree = ET.parse(filepath)
-        xml_data = tree.getroot()
-        xmlstr = ET.tostring(xml_data, encoding='utf-8', method='xml')
-        data = xmltodict.parse(xmlstr)
-        data = data['collection']['document']
+        with open(filepath, encoding="utf8") as file:
 
-        if self.config.schema == "source":
-            for uid, doc in enumerate(data):
-                out = {
-                    'id': doc['id'],
-                    'passage': {
-                        'offset': doc['passage']['offset'],
-                        'text': doc['passage']['text'],
-                        'annotation': [],
-                        'relation': [],
+            tree = ET.parse(file)
+            xml_data = tree.getroot()
+            xmlstr = ET.tostring(xml_data, encoding='utf-8', method='xml')
+            data = xmltodict.parse(xmlstr)
+            data = data['collection']['document']
+
+            if self.config.schema == "source":
+
+                for uid, doc in enumerate(data):
+                    out = {
+                        'id': doc['id'],
+                        'passage': {
+                            'offset': doc['passage']['offset'],
+                            'text': doc['passage']['text'],
+                            'annotation': [],
+                            'relation': [],
+                        }
                     }
-                }
 
-                for annotation in doc['passage']['annotation']:
-                    anno = {
-                        '@id': annotation['@id'],
-                        'infon': [],
-                        'location': [],
-                        'text': annotation['text'],
+                    for annotation in doc['passage']['annotation']:
+                        anno = {
+                            '@id': annotation['@id'],
+                            'infon': [],
+                            'text': annotation['text'],
+                        }
+
+                        for infon in annotation['infon']:
+                            anno['infon'].append({
+                                '@key': infon['@key'],
+                                '#text': infon['#text'],
+                            })
+
+                        if isinstance(annotation['location'], list):
+                            split_location = []
+                            for location in annotation['location']:
+                                split_location.append({
+                                    '@offset': location['@offset'],
+                                    '@length': location['@length'],
+                                })
+                            anno['location'] = split_location
+                        elif isinstance(annotation['location'], collections.OrderedDict):
+                            anno['location'] = [{
+                                '@offset': location['@offset'],
+                                '@length': location['@length'],
+                            }]
+
+                        out['passage']['annotation'].append(anno)
+
+                    for relation in doc['passage']['relation']:
+                        rel = {
+                            "@id": relation["@id"],
+                            "infon": [],
+                        }
+
+                        for infon in relation['infon']:
+                            rel['infon'].append({
+                                '@key': infon['@key'],
+                                '#text': infon['#text'],
+                            })
+
+                        rel['node'] = {
+                            '@refid': relation['node']['@refid'],
+                            '@role': relation['node']['@role'],
+                        }
+                        out['passage']['relation'].append(rel)
+
+                    yield uid, out
+
+            elif self.config.schema == "bigbio_kb":
+                for uid, doc in enumerate(data):
+                    out = {
+                        'id': uid,
+                        'document_id': doc['id'],
+                        'passages': [],
+                        'entities': [],
+                        'events': [],
+                        'coreferences': [],
+                        'relations': [],
                     }
-                    for infon in anno['infon']:
-                        anno['infon'].append({
-                            '@key': infon['@key'],
-                            '#text': infon['#text'],
+
+                    # pass sentences to passages
+                    for i, passage in enumerate(doc['passage']['text'].split('\n')):
+                        # match the offsets on the text after removing \n
+                        if i == 0:
+                            marker = 0
+                        else:
+                            marker = out['passages'][-1]['offsets'][-1][-1] + 1
+
+                        out['passages'].append({
+                            'id': f'{uid}-{i}',
+                            'text': [passage],
+                            'type': 'sentence',
+                            'offsets': [[marker, marker + len(passage)]],
                         })
-                    for location in anno['location']:
-                        anno['location'].append({
-                            '@offset': location['@offset'],
-                            '#text': location['#text'],
-                        })
-                    out['passage']['annotation'].append(anno)
 
-                for relation in doc['passage']['relation']:
-                    rel = {
-                        "@id": relation["@id"],
-                        "infon": [],
-                        "node": [],
-                    }
-                    for infon in relation['infon']:
-                        relation['infon'].append({
-                            '@key': infon['@key'],
-                            '#text': infon['#text'],
-                        })
-                    for node in relation['node']:
-                        relation['node'].append({
-                            '@refid': node['@refid'],
-                            '@role': node['@role'],
-                        })
-                    out['passage']['relation'].append(relation)
-
-                yield uid, out
-
-        elif self.config.schema == "bigbio_kb":
-            for uid, doc in enumerate(data):
-                out = {
-                    'id': uid,
-                    'document_id': doc['id'],
-                    'passages': [],
-                    'entities': [],
-                    'events': [],
-                    'coreferences': [],
-                    'relations': [],
-                }
-
-                # pass sentences to passages
-                for i, passage in enumerate(doc['passage']['text'].split('\n')):
-                    # match the offsets on the text after removing \n
-                    if i == 0:
-                        marker = 0
-                    else:
-                        marker = out['passages'][-1]['offsets'][-1][-1] + 1
-
-                    out['passages'].append({
-                        'id': f'{uid}-{i}',
-                        'text': [passage],
-                        'type': 'sentence',
-                        'offsets': [[marker, marker + len(passage)]],
-                    })
-
-                for ent in doc['passage']['annotation']:
-                    offsets = []
-                    # handle entities splitted across the text
-                    if isinstance(ent['location'], list):
-                        for part in ent['location']:
-                            start = int(part['@offset'])
-                            end = int(part['@offset']) + int(part['@length'])
+                    for ent in doc['passage']['annotation']:
+                        offsets = []
+                        text_s = []
+                        # handle entities splitted across the text
+                        if isinstance(ent['location'], list):
+                            for part in ent['location']:
+                                start = int(part['@offset'])
+                                end = int(part['@offset']) + int(part['@length'])
+                                offsets.append([start, end])
+                                text_s.append(doc['passage']['text'][start:end])
+                        # handle entities where all characters are consecutive
+                        elif isinstance(ent['location'], collections.OrderedDict):
+                            start = int(ent['location']['@offset'])
+                            end = int(ent['location']['@offset']) + int(ent['location']['@length'])
                             offsets.append([start, end])
-                    # handle entities where all characters are consecutive
-                    elif isinstance(ent['location'], collections.OrderedDict):
-                        start = int(ent['location']['@offset'])
-                        end = int(ent['location']['@offset']) + int(ent['location']['@length'])
-                        offsets.append([start, end])
+                            text_s.append(ent['text'])
 
-                    out['entities'].append({
-                        'id': ent['@id'],
-                        'type': ent['infon'][1]['#text'],
-                        'text': [ent['text']],
-                        'offsets': offsets,
-                        'normalized': [],
-                    })
+                        out['entities'].append({
+                            'id': f'{uid}-{ent["@id"]}',
+                            'type': ent['infon'][1]['#text'],
+                            'text': text_s,
+                            'offsets': offsets,
+                            'normalized': [{
+                                "db_name": "",
+                                "db_id": "",
+                            }],
+                        })
 
-                yield uid, out
+                    yield uid, out
 
 
 if __name__ == "__main__":
-    datasets.load_dataset(__file__)
+    datasets.load_dataset(path = 'bronco.py', data_dir=r"C:\Users\admin\Desktop\BRONCO150", name="bronco_source")
