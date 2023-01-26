fae69ce038025f4a33fdfe606093ad7b8b5d9428
Ignacio Llorca <llorcarodriguez@uni-potsdam.de>
2023-01-26 13:55:01 +0100

    Use bioC parser instead of vanilla

--- bigbio/biodatasets/bronco/bronco.py
+++ bigbio/biodatasets/bronco/bronco.py
@@ -15,17 +15,13 @@
 
 import os
 from typing import List, Tuple, Dict
+from bioc import biocxml
 
 import datasets
-from .bigbiohub import BigBioConfig
-from bigbio.utils.constants import Tasks, Lang
-from bigbio.utils.license import Licenses
+from bigbiohub import BigBioConfig
+from bigbiohub import kb_features
+from bigbio.utils.constants import Tasks
 
-import xml.etree.ElementTree as ET
-import xmltodict
-import collections
-
-from .bigbiohub import kb_features
 
 _LOCAL = True
 _CITATION = """\
@@ -48,7 +44,7 @@ _DESCRIPTION = """\
 BRONCO150 is a corpus containing selected sentences of 150 German discharge summaries of cancer patients (hepatocelluar carcinoma or melanoma) treated at Charite Universitaetsmedizin Berlin or Universitaetsklinikum Tuebingen. All discharge summaries were manually anonymized. The original documents were scrambled at the sentence level to make reconstruction of individual reports impossible.
 """
 _HOMEPAGE = "https://www2.informatik.hu-berlin.de/~leser/bronco/index.html"
-_LICENSE = Licenses.DUA
+#_LICENSE = Licenses.DUA
 _URLS = {}
 _PUBMED = False
 _SUPPORTED_TASKS = [Tasks.NAMED_ENTITY_RECOGNITION, Tasks.NAMED_ENTITY_DISAMBIGUATION]
@@ -56,16 +52,13 @@ _SOURCE_VERSION = "1.0.0"
 _BIGBIO_VERSION = "1.0.0"
 _DATASETNAME = "bronco"
 _DISPLAYNAME = "BRONCO"
-_LANGUAGES = [Lang.DE]
+_LANGUAGES = 'German'
 
 
 class Bronco(datasets.GeneratorBasedBuilder):
     SOURCE_VERSION = datasets.Version(_SOURCE_VERSION)
     BIGBIO_VERSION = datasets.Version(_BIGBIO_VERSION)
     DEFAULT_CONFIG_NAME = "bronco_bigbio_kb"
-
-    # ds_bigbio = datasets.load_dataset('my_dataset', name='bigbio', data_dir="/path/to/data/files")
-
     BUILDER_CONFIGS = [
         BigBioConfig(
             name="bronco_source",
@@ -83,35 +76,25 @@ class Bronco(datasets.GeneratorBasedBuilder):
         ),
     ]
 
-    DEFAULT_CONFIG_NAME = "[dataset_name]_source"
-
     def _info(self) -> datasets.DatasetInfo:
-
-        # Create the source schema; this schema will keep all keys/information/labels as close to the original dataset as possible.
-
-        # You can arbitrarily nest lists and dictionaries.
-        # For iterables, use lists over tuples or `datasets.Sequence`
-
         if self.config.schema == "source":
             features = datasets.Features(
                 {
                     "id": datasets.Value("string"),
                     "passage": {
-                        "offset": datasets.Value("string"),
+                        "offset": datasets.Value("int32"),
                         "text": datasets.Value("string"),
                         "annotation": [
                             {
-                                "@id": datasets.Value("string"),
-                                "infon": [
-                                    {
-                                        "@key": datasets.Value("string"),
-                                        "#text": datasets.Value("string"),
-                                    }
-                                ],
+                                "id": datasets.Value("string"),
+                                "infon": {
+                                    "file": datasets.Value("string"),
+                                    "type": datasets.Value("string"),
+                                },
                                 "location": [
                                     {
-                                        "@offset": datasets.Value("string"),
-                                        "@length": datasets.Value("string"),
+                                        "offset": datasets.Value("int32"),
+                                        "length": datasets.Value("int32"),
                                     }
                                 ],
                                 "text": datasets.Value("string"),
@@ -119,17 +102,19 @@ class Bronco(datasets.GeneratorBasedBuilder):
                         ],
                         "relation": [
                             {
-                                "@id": datasets.Value("string"),
-                                "infon": [
+                                "id": datasets.Value("string"),
+                                "infon": {
+                                            "file": datasets.Value("string"),
+                                            "type": datasets.Value("string"),
+                                            "norm/atr": datasets.Value("string"),
+                                            "string": datasets.Value("string"),
+                                         },
+                                "node": [
                                     {
-                                        "@key": datasets.Value("string"),
-                                        "#text": datasets.Value("string"),
+                                        "refid": datasets.Value("string"),
+                                        "role": datasets.Value("string"),
                                     }
                                 ],
-                                "node": {
-                                    "@refid": datasets.Value("string"),
-                                    "@role": datasets.Value("string"),
-                                },
                             }
                         ],
                     }
@@ -152,6 +137,7 @@ class Bronco(datasets.GeneratorBasedBuilder):
             raise ValueError("This is a local dataset. Please pass the data_dir kwarg to load_dataset.")
         else:
             data_dir = self.config.data_dir
+
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
@@ -164,73 +150,66 @@ class Bronco(datasets.GeneratorBasedBuilder):
 
     def _generate_examples(self, filepath, split: str) -> Tuple[int, Dict]:
         """Yields examples as (key, example) tuples."""
-
-        with open(filepath, encoding="utf8") as file:
-
-            tree = ET.parse(file)
-            xml_data = tree.getroot()
-            xmlstr = ET.tostring(xml_data, encoding='utf-8', method='xml')
-            data = xmltodict.parse(xmlstr)
-            data = data['collection']['document']
+        with open(filepath, "r") as fp:
+            data = biocxml.load(fp).documents
 
             if self.config.schema == "source":
-
                 for uid, doc in enumerate(data):
                     out = {
-                        'id': doc['id'],
+                        'id': doc.id,
                         'passage': {
-                            'offset': doc['passage']['offset'],
-                            'text': doc['passage']['text'],
+                            'offset': doc.passages[0].offset,
+                            'text': doc.passages[0].text,
                             'annotation': [],
                             'relation': [],
                         }
                     }
 
-                    for annotation in doc['passage']['annotation']:
+                    # handle entities
+                    for annotation in doc.passages[0].annotations:
                         anno = {
-                            '@id': annotation['@id'],
-                            'infon': [],
-                            'text': annotation['text'],
+                            'id': annotation.id,
+                            'infon': annotation.infons,
+                            'text': annotation.text,
+                            'location': [],
                         }
-
-                        for infon in annotation['infon']:
-                            anno['infon'].append({
-                                '@key': infon['@key'],
-                                '#text': infon['#text'],
+                        for location in annotation.locations:
+                            anno['location'].append({
+                                'offset': location.offset,
+                                'length': location.length,
                             })
-
-                        if isinstance(annotation['location'], list):
-                            split_location = []
-                            for location in annotation['location']:
-                                split_location.append({
-                                    '@offset': location['@offset'],
-                                    '@length': location['@length'],
-                                })
-                            anno['location'] = split_location
-                        elif isinstance(annotation['location'], collections.OrderedDict):
-                            anno['location'] = [{
-                                '@offset': location['@offset'],
-                                '@length': location['@length'],
-                            }]
-
                         out['passage']['annotation'].append(anno)
 
-                    for relation in doc['passage']['relation']:
+                    # handle relations
+                    for relation in doc.passages[0].relations:
                         rel = {
-                            "@id": relation["@id"],
-                            "infon": [],
+                            "id": relation.id,
+                            "node": [],
                         }
 
-                        for infon in relation['infon']:
-                            rel['infon'].append({
-                                '@key': infon['@key'],
-                                '#text': infon['#text'],
+                        # relation.infons has different keys depending on the relation type
+                        # these must be unified to comply with a fixed schema
+                        if relation.infons['type'] == 'Normalization':
+                            rel['infon'] = {
+                                'file': relation.infons['file'],
+                                'type': relation.infons['type'],
+                                'norm/atr': relation.infons['normalization type'],
+                                'string': relation.infons['string']
+                            }
+                        else:
+                            rel['infon'] = {
+                                'file': relation.infons['file'],
+                                'type': relation.infons['type'],
+                                'norm/atr': relation.infons['attribute type'],
+                                'string': '',
+                            }
+
+                        for node in relation.nodes:
+                            rel['node'].append({
+                                'refid': node.refid,
+                                'role': node.role,
                             })
 
-                        rel['node'] = {
-                            '@refid': relation['node']['@refid'],
-                            '@role': relation['node']['@role'],
-                        }
                         out['passage']['relation'].append(rel)
 
                     yield uid, out
@@ -239,7 +218,7 @@ class Bronco(datasets.GeneratorBasedBuilder):
                 for uid, doc in enumerate(data):
                     out = {
                         'id': uid,
-                        'document_id': doc['id'],
+                        'document_id': doc.id,
                         'passages': [],
                         'entities': [],
                         'events': [],
@@ -247,14 +226,14 @@ class Bronco(datasets.GeneratorBasedBuilder):
                         'relations': [],
                     }
 
-                    # catch all normalized entities
+                    # catch all normalized entities for lookup
                     norm_map = {}
-                    for rel in doc['passage']['relation']:
-                        if rel['infon'][-1]['#text'] == 'Normalization':
-                            norm_map[rel['node']['@role']] = rel['node']['@refid']
+                    for rel in doc.passages[0].relations:
+                        if rel.infons['type'] == 'Normalization':
+                            norm_map[rel.nodes[0].role] = rel.nodes[0].refid
 
-                    # pass sentences to passages
-                    for i, passage in enumerate(doc['passage']['text'].split('\n')):
+                    # handle passages - split text into sentences
+                    for i, passage in enumerate(doc.passages[0].text.split('\n')):
                         # match the offsets on the text after removing \n
                         if i == 0:
                             marker = 0
@@ -269,32 +248,22 @@ class Bronco(datasets.GeneratorBasedBuilder):
                         })
 
                     # handle entities
-                    for ent in doc['passage']['annotation']:
+                    for ent in doc.passages[0].annotations:
                         offsets = []
                         text_s = []
-                        # handle entities splitted across the text
-                        if isinstance(ent['location'], list):
-                            for part in ent['location']:
-                                start = int(part['@offset'])
-                                end = int(part['@offset']) + int(part['@length'])
-                                offsets.append([start, end])
-                                text_s.append(doc['passage']['text'][start:end])
-                        # handle entities where all characters are consecutive
-                        elif isinstance(ent['location'], collections.OrderedDict):
-                            start = int(ent['location']['@offset'])
-                            end = int(ent['location']['@offset']) + int(ent['location']['@length'])
-                            offsets.append([start, end])
-                            text_s.append(ent['text'])
+                        for loc in ent.locations:
+                            offsets.append([loc.offset, loc.offset + loc.length])
+                            text_s.append(doc.passages[0].text[loc.offset:loc.offset + loc.length])
 
                         out['entities'].append({
-                            'id': f'{uid}-{ent["@id"]}',
-                            'type': ent['infon'][1]['#text'],
+                            'id': f'{uid}-{ent.id}',
+                            'type': ent.infons['type'],
                             'text': text_s,
                             'offsets': offsets,
                             'normalized': [{
-                                'db_name': norm_map.get(ent["@id"], ':').split(':')[0],
+                                'db_name': norm_map.get(ent.id, ':').split(':')[0],
                                 # replace faulty connectors in db_ids
-                                'db_id': norm_map.get(ent["@id"], ':').split(':')[1].replace(",",".").replace("+",""),
+                                'db_id': norm_map.get(ent.id, ':').split(':')[1].replace(",", ".").replace("+", ""),
                             }]
                         })
 
@@ -302,4 +271,6 @@ class Bronco(datasets.GeneratorBasedBuilder):
 
 
 if __name__ == "__main__":
-    datasets.load_dataset(path = 'bronco.py', data_dir=r"C:\Users\admin\Desktop\BRONCO150", name="bronco_source")
+    datasets.load_dataset(path = 'bronco.py',
+                          data_dir=r"C:\Users\admin\Desktop\BRONCO150",
+                          name="bronco_source")
